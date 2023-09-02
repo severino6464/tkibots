a3a0a219e583ceea5b9dc6c9a76069068ca62ddd
severino6464 <46749707+severino6464@users.noreply.github.com>
2023-09-02 15:26:25 -0300

    Add files via upload

---
+++ sala194.py
@@ -0,0 +1,51 @@
+import telebot
+import time
+import datetime
+import random
+
+CHAVE_API = "5597794728:AAGfwOg3RijfPrQ5S_Iw6NKAuYucNEdIsO8" # BOT FOX
+
+bot = telebot.TeleBot(CHAVE_API)
+
+group_id = '-1001908356005'
+
+
+
+
+
+mensagem = """
+🚨 ENTRADA CONFIRMADA 🚨
+
+🐯 Fortune Tiger 
+⏰ Estratégia: Horários Pagantes
+⚠️ Válido ate: {}
+
+💰 {}x Normal
+💰 {}x Turbo
+
+⚡ Intercalando
+
+[🔗 Fazer CADASTRO ✅](https://affiliates.nuts.bet/visit/?bta=36237&brand=nutsbet)
+[🔗 Abrir FORTUNE TIGER](https://nuts.bet/casino/game/2180615)
+
+"""
+
+
+
+while True:
+
+    print("aff194")
+
+ 
+
+
+    n_jogadas = random.randint(6, 20)
+    n_jogadas2 = random.randint(4, 20)
+    validade = datetime.datetime.now() + datetime.timedelta(minutes=10)
+    hora_validade = validade.strftime("%H:%M")
+    mensagem_formatada = mensagem.format(hora_validade,n_jogadas, n_jogadas2)
+
+    bot.send_message(chat_id=group_id, text=mensagem_formatada, parse_mode='Markdown')
+
+    time.sleep(600)  # Espera 10 minutos (600 segundos)
+
