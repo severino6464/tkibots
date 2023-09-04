5ef39765a5ccacfc16819a09a8ee59fb2e7aa1f0
severino6464 <46749707+severino6464@users.noreply.github.com>
2023-09-04 16:36:07 -0300

    Atualização do arquivo

--- sala227.py
+++ sala227.py
@@ -41,25 +41,21 @@ mensagem = """
 [📱 Jogar Fortune Tiger🐯☘](https://affiliates.nuts.bet/visit/?bta=35976&brand=nutsbet)
 """
 
+print("BOT-aff224-nuts")
 
-
-while True:
-
-    print("BOT-aff224-nuts")
-
-    bot.send_message(chat_id=group_id, text=texto4, parse_mode='Markdown')
-    time.sleep(120) 
+bot.send_message(chat_id=group_id, text=texto4, parse_mode='Markdown')
+time.sleep(120) 
 
 
 
-    n_jogadas = random.randint(2, 10)
-    validade = datetime.datetime.now() + datetime.timedelta(minutes=10)
-    hora_validade = validade.strftime("%H:%M")
-    mensagem_formatada = mensagem.format(n_jogadas, hora_validade, n_jogadas)
+n_jogadas = random.randint(2, 10)
+validade = datetime.datetime.now() + datetime.timedelta(minutes=10)
+hora_validade = validade.strftime("%H:%M")
+mensagem_formatada = mensagem.format(n_jogadas, hora_validade, n_jogadas)
 
-    bot.send_message(chat_id=group_id, text=mensagem_formatada, parse_mode='Markdown')
+bot.send_message(chat_id=group_id, text=mensagem_formatada, parse_mode='Markdown')
 
-    time.sleep(60)  # Espera 5 minutos (300 segundos)
+time.sleep(60)  # Espera 5 minutos (300 segundos)
 
-    bot.send_message(chat_id=group_id, text=texto5, parse_mode='Markdown')
-    time.sleep(120) 
+bot.send_message(chat_id=group_id, text=texto5, parse_mode='Markdown')
+time.sleep(120) 
