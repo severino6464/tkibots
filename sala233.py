0dd9a53276d317b76d0c9461c06ef9f1afe9e01d
severino6464 <46749707+severino6464@users.noreply.github.com>
2023-09-05 18:37:33 -0300

    Update sala233.py

---
+++ sala233.py
@@ -0,0 +1,63 @@
+import telebot
+import time
+import datetime
+import random
+
+CHAVE_API = "5597794728:AAGfwOg3RijfPrQ5S_Iw6NKAuYucNEdIsO8" # BOT FOX
+
+bot = telebot.TeleBot(CHAVE_API)
+
+group_id = '-1001892928680'
+
+
+texto4 = """
+🎲 Fique atento ao jogo 🎲
+
+🐭 Fortune mouse
+🔎 Estamos validando uma entrada
+
+[📱 Cadastre-se aqui](https://affiliates.nuts.bet/visit/?bta=36367&brand=nutsbet)
+"""
+
+texto5 = """
+🔷🔹 Entrada Finalizada 🔹🔷
+     ✅✅ GRENN! ✅✅
+ 
+"""
+
+
+mensagem = """
+⚠️ ENTRADA CONFIRMADA ⚠️
+
+🐭 Fortune mouse
+
+🔥 𝗡º 𝗱𝗲 𝗝𝗼𝗴𝗮𝗱𝗮𝘀: {}
+⏰ Sinal válido até: {}
+
+🌪 Faça no máximo {} jogadas!
+
+[📱 Cadastre-se aqui](https://affiliates.nuts.bet/visit/?bta=36367&brand=nutsbet)
+
+[📱 Jogar Fortune mouse 🐭 ](https://affiliates.nuts.bet/visit/?bta=36367&brand=nutsbet)
+"""
+
+
+
+print("aff233")
+
+bot.send_message(chat_id=group_id, text=texto4, parse_mode='Markdown')
+time.sleep(60) 
+
+
+
+n_jogadas = random.randint(2, 15)
+validade = datetime.datetime.now() + datetime.timedelta(minutes=2)
+hora_validade = validade.strftime("%H:%M")
+mensagem_formatada = mensagem.format(n_jogadas, hora_validade, n_jogadas)
+
+bot.send_message(chat_id=group_id, text=mensagem_formatada, parse_mode='Markdown')
+
+time.sleep(120)  # Espera 5 minutos (300 segundos)
+
+bot.send_message(chat_id=group_id, text=texto5, parse_mode='Markdown')
+time.sleep(120) 
