d8a9d28eff0b1473519d182bd3435f7ff1af8158
severino6464 <46749707+severino6464@users.noreply.github.com>
2023-09-07 17:20:08 -0300

    Atualização do arquivo

--- sala251.py
+++ sala251.py
@@ -44,7 +44,7 @@ mensagem = """
 
 
 
-print("aff")
+print("aff251")
 possibilidade_mina_aleatoria = random.choice(possibilidades_minas)
 link_aleatorio = random.choice(links)
 validade = datetime.datetime.now() + datetime.timedelta(minutes=5)
