e671282370349de439e8fd08990cacf2482b085b
severino6464 <46749707+severino6464@users.noreply.github.com>
2023-09-13 13:30:46 -0300

    Atualização do arquivo

--- sala229.py
+++ sala229.py
@@ -47,52 +47,36 @@ def send_signal():
     bot.send_message(chat_id=channel_id, text=texto4, parse_mode='Markdown')
     time.sleep(120) 
 
-
     n_jogadas = random.randint(6, 20)
     n_jogadas2 = random.randint(4, 20)
     validade = datetime.datetime.now() + datetime.timedelta(minutes=10)
     hora_validade = validade.strftime("%H:%M")
-    mensagem_formatada = mensagem.format(hora_validade,n_jogadas, n_jogadas2)
-
+    mensagem_formatada = mensagem.format(n_jogadas, hora_validade, n_jogadas2)
+    bot.send_message(chat_id=channel_id, text=mensagem_formatada, parse_mode='Markdown')
     time.sleep(600)
 
-    bot.send_message(chat_id=channel_id, text=texto4, parse_mode='Markdown')
-    time.sleep(120) 
-
-
     n_jogadas = random.randint(6, 20)
     n_jogadas2 = random.randint(4, 20)
     validade = datetime.datetime.now() + datetime.timedelta(minutes=10)
     hora_validade = validade.strftime("%H:%M")
-    mensagem_formatada = mensagem.format(hora_validade,n_jogadas, n_jogadas2)
-
+    mensagem_formatada = mensagem.format(n_jogadas, hora_validade, n_jogadas2)
+    bot.send_message(chat_id=channel_id, text=mensagem_formatada, parse_mode='Markdown')
     time.sleep(600)
 
-    bot.send_message(chat_id=channel_id, text=texto4, parse_mode='Markdown')
-    time.sleep(120) 
-
-
     n_jogadas = random.randint(6, 20)
     n_jogadas2 = random.randint(4, 20)
     validade = datetime.datetime.now() + datetime.timedelta(minutes=10)
     hora_validade = validade.strftime("%H:%M")
-    mensagem_formatada = mensagem.format(hora_validade,n_jogadas, n_jogadas2)
-
+    mensagem_formatada = mensagem.format(n_jogadas, hora_validade, n_jogadas2)
+    bot.send_message(chat_id=channel_id, text=mensagem_formatada, parse_mode='Markdown')
     time.sleep(600)
 
     bot.send_message(chat_id=channel_id, text=texto5, parse_mode='Markdown')
-   
-
-
-
-
-
-
 
 def check_and_send_signal():
     current_time = datetime.datetime.now().strftime("%H:%M")
     signal_times = [
-        "10:00", "14:00", "23:00" 
+        "10:00", "13:30", "23:00" 
     ]
 
     if current_time in signal_times:
