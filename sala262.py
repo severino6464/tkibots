5e6480c70fbe2eb0d847e2239371b9ad00669531
severino6464 <46749707+severino6464@users.noreply.github.com>
2023-09-14 10:12:04 -0300

    Atualização do arquivo

--- sala262.py
+++ sala262.py
@@ -7,7 +7,7 @@ CHAVE_API = "5597794728:AAGfwOg3RijfPrQ5S_Iw6NKAuYucNEdIsO8" # BOT FOX
 
 bot = telebot.TeleBot(CHAVE_API)
 
-group_id = '-1001913792567'
+group_id = '-1001957104133'
 
 possibilidades_minas = [
 "‼️ RETIRAR EM 1.50x",
