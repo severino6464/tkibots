60cb0e3c242f5280db679f5ec125b18986dd779a
severino6464 <46749707+severino6464@users.noreply.github.com>
2023-09-15 16:28:22 -0300

    Atualização do arquivo

---
+++ sala306.py
@@ -0,0 +1,124 @@
+import telebot
+import datetime
+import random
+import time
+
+
+CHAVE_API = "5597794728:AAGfwOg3RijfPrQ5S_Iw6NKAuYucNEdIsO8" 
+
+bot = telebot.TeleBot(CHAVE_API)
+
+channel_id = '-1001914085821'  
+
+possibilidades_minas = [
+"‼️ RETIRAR EM 1.50x",
+"‼️ RETIRAR EM 2.00x",
+"‼️ RETIRAR EM 2.00x",
+"‼️ RETIRAR EM 3.50x",
+"‼️ RETIRAR EM 1.04x",
+"‼️ RETIRAR EM 2.30x",
+"‼️ RETIRAR EM 5.00x"
+]
+
+
+
+texto4 = """
+⚠️ Fique atento ao jogo ⚠️
+
+✈️ Aviator 
+🔎 identificando entrada
+
+🖥 Link de cadastro:[Clique aqui](https://affiliates.nuts.bet/visit/?bta=36355&brand=nutsbet&utm_campaign=aviator)
+"""
+
+texto5 = """
+SESSÃO ENCERRADA!
+"""
+
+
+mensagem = """
+✅ Entrada Confirmada 
+
+{}
+
+⚠️ MÁXIMO 2 GALES 
+🔔 Entrada Confirmada 🔔  
+✅ Entrar Agora  
+
+⏱️ Válido até: {}
+
+📲: Plataforma correta: [Clique aqui](https://affiliates.nuts.bet/visit/?bta=36355&brand=nutsbet&utm_campaign=aviator))
+"""
+
+def send_signal():
+
+    bot.send_message(chat_id=channel_id, text=texto4, parse_mode='Markdown')
+    time.sleep(120) 
+
+
+    possibilidade_mina_aleatoria = random.choice(possibilidades_minas)
+    validade = datetime.datetime.now() + datetime.timedelta(minutes=5)
+    hora_validade = validade.strftime("%H:%M")
+    mensagem_formatada = mensagem.format(possibilidade_mina_aleatoria, hora_validade)
+    bot.send_message(chat_id=channel_id, text=mensagem_formatada, parse_mode='Markdown')
+    time.sleep(600)
+
+    possibilidade_mina_aleatoria = random.choice(possibilidades_minas)
+    validade = datetime.datetime.now() + datetime.timedelta(minutes=5)
+    hora_validade = validade.strftime("%H:%M")
+    mensagem_formatada = mensagem.format(possibilidade_mina_aleatoria, hora_validade)
+    bot.send_message(chat_id=channel_id, text=mensagem_formatada, parse_mode='Markdown')
+    time.sleep(600)
+
+    possibilidade_mina_aleatoria = random.choice(possibilidades_minas)
+    validade = datetime.datetime.now() + datetime.timedelta(minutes=5)
+    hora_validade = validade.strftime("%H:%M")
+    mensagem_formatada = mensagem.format(possibilidade_mina_aleatoria, hora_validade)
+    bot.send_message(chat_id=channel_id, text=mensagem_formatada, parse_mode='Markdown')
+    time.sleep(600)
+
+    possibilidade_mina_aleatoria = random.choice(possibilidades_minas)
+    validade = datetime.datetime.now() + datetime.timedelta(minutes=5)
+    hora_validade = validade.strftime("%H:%M")
+    mensagem_formatada = mensagem.format(possibilidade_mina_aleatoria, hora_validade)
+    bot.send_message(chat_id=channel_id, text=mensagem_formatada, parse_mode='Markdown')
+    time.sleep(600)
+
+    possibilidade_mina_aleatoria = random.choice(possibilidades_minas)
+    validade = datetime.datetime.now() + datetime.timedelta(minutes=5)
+    hora_validade = validade.strftime("%H:%M")
+    mensagem_formatada = mensagem.format(possibilidade_mina_aleatoria, hora_validade)
+    bot.send_message(chat_id=channel_id, text=mensagem_formatada, parse_mode='Markdown')
+    time.sleep(600)
+
+    possibilidade_mina_aleatoria = random.choice(possibilidades_minas)
+    validade = datetime.datetime.now() + datetime.timedelta(minutes=5)
+    hora_validade = validade.strftime("%H:%M")
+    mensagem_formatada = mensagem.format(possibilidade_mina_aleatoria, hora_validade)
+    bot.send_message(chat_id=channel_id, text=mensagem_formatada, parse_mode='Markdown')
+    time.sleep(600)
+
+    
+    bot.send_message(chat_id=channel_id, text=texto5, parse_mode='Markdown')
+
+
+
+
+
+
+def check_and_send_signal():
+    current_time = datetime.datetime.now().strftime("%H:%M")
+    signal_times = [
+        "10:00", "15:30", "20:30"
+    ]
+
+    if current_time in signal_times:
+        send_signal()
+
+
+try:
+        check_and_send_signal()
+        # Wait for 1 minute before checking the time again
+        datetime.datetime.now() + datetime.timedelta(minutes=0)
+except Exception as e:
+        print(f"Error occurred: {str(e)}")
