4556b3ee63421eee39cf44d09e15f8b81513a6f2
severino6464 <46749707+severino6464@users.noreply.github.com>
2023-09-16 23:29:33 -0300

    Atualização do arquivo

--- sala121.py
+++ sala121.py
@@ -30,21 +30,15 @@ mensagem = """
 
 """
 
-
-
-while True:
-
-    print("122")
-
  
 
 
-    n_jogadas = random.randint(6, 20)
-    validade = datetime.datetime.now() + datetime.timedelta(minutes=10)
-    hora_validade = validade.strftime("%H:%M")
-    mensagem_formatada = mensagem.format(hora_validade,n_jogadas, n_jogadas)
+n_jogadas = random.randint(6, 20)
+validade = datetime.datetime.now() + datetime.timedelta(minutes=10)
+hora_validade = validade.strftime("%H:%M")
+mensagem_formatada = mensagem.format(hora_validade,n_jogadas, n_jogadas)
 
-    bot.send_message(chat_id=group_id, text=mensagem_formatada, parse_mode='Markdown')
+bot.send_message(chat_id=group_id, text=mensagem_formatada, parse_mode='Markdown')
 
-    time.sleep(600)  # Espera 10 minutos (600 segundos)
+time.sleep(600)  # Espera 10 minutos (600 segundos)
 
