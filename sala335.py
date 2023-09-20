7f6410573d70e780c01a1a1b89f4bfa93eac0d57
severino6464 <46749707+severino6464@users.noreply.github.com>
2023-09-20 11:40:41 -0300

    Atualização do arquivo

--- sala335.py
+++ sala335.py
@@ -39,8 +39,8 @@ print("aff335")
  
 
 
-n_jogadas = random.randint(6, 20)
-n_jogadas2 = random.randint(4, 20)
+n_jogadas = random.randint(1, 10)
+n_jogadas2 = random.randint(1, 10)
 validade = datetime.datetime.now() + datetime.timedelta(minutes=10)
 hora_validade = validade.strftime("%H:%M")
 mensagem_formatada = mensagem.format(hora_validade,n_jogadas, n_jogadas2)
