953136980982ea768ab8bcc142fb40a404a2aed4
severino6464 <46749707+severino6464@users.noreply.github.com>
2023-09-20 17:52:11 -0300

    Atualização do arquivo

---
+++ sala372.py
@@ -0,0 +1,107 @@
+import telebot
+import datetime
+import random
+import time
+
+
+CHAVE_API = "5597794728:AAGfwOg3RijfPrQ5S_Iw6NKAuYucNEdIsO8" 
+
+bot = telebot.TeleBot(CHAVE_API)
+
+channel_id = '-1001907800084'  
+
+possibilidades_minas = [
+    "Apostar em números baixos [1-18]",
+    "Apostar em números altos [19-36]",
+    "Apostar na duzia 1 e 3",
+    "Apostar na cor 🔴",
+    "Apostar na cor ⚫"
+]
+
+
+
+texto4 = """
+ATENÇÃO VAMOS INICIAR 
+
+FAÇA SEU CADASTRO 👇
+[Clique aqui](https://affiliates.nuts.bet/visit/?bta=36824&brand=nutsbet)
+
+ENTRE NO JOGO👇🏻
+[Clique aqui](https://affiliates.nuts.bet/visit/?bta=36824&brand=nutsbet)
+"""
+
+texto5 = """
+SESSÃO ENCERRADA!
+"""
+
+
+mensagem = """
+💰 ENTRADA CONFIRMADA 💰
+🎰 Roleta: Brasileira
+Link: [Clique aqui](https://affiliates.nuts.bet/visit/?bta=36824&brand=nutsbet)
+
+{}
+
+👉 Cobrir o zero
+🔁 Fazer até 3 gales
+🔗 [Cadastre-se antes de Jogar!](https://affiliates.nuts.bet/visit/?bta=36824&brand=nutsbet)
+⏱️ Válido até: {}
+"""
+
+def send_signal():
+
+    bot.send_message(chat_id=channel_id, text=texto4, parse_mode='Markdown')
+    time.sleep(120) 
+
+
+    possibilidade_mina_aleatoria = random.choice(possibilidades_minas)
+    validade = datetime.datetime.now() + datetime.timedelta(minutes=5)
+    hora_validade = validade.strftime("%H:%M")
+    mensagem_formatada = mensagem.format(possibilidade_mina_aleatoria, hora_validade)
+    bot.send_message(chat_id=channel_id, text=mensagem_formatada, parse_mode='Markdown')
+    time.sleep(1798)
+
+    possibilidade_mina_aleatoria = random.choice(possibilidades_minas)
+    validade = datetime.datetime.now() + datetime.timedelta(minutes=5)
+    hora_validade = validade.strftime("%H:%M")
+    mensagem_formatada = mensagem.format(possibilidade_mina_aleatoria, hora_validade)
+    bot.send_message(chat_id=channel_id, text=mensagem_formatada, parse_mode='Markdown')
+    time.sleep(1800)
+
+    possibilidade_mina_aleatoria = random.choice(possibilidades_minas)
+    validade = datetime.datetime.now() + datetime.timedelta(minutes=5)
+    hora_validade = validade.strftime("%H:%M")
+    mensagem_formatada = mensagem.format(possibilidade_mina_aleatoria, hora_validade)
+    bot.send_message(chat_id=channel_id, text=mensagem_formatada, parse_mode='Markdown')
+    time.sleep(1800)
+
+    possibilidade_mina_aleatoria = random.choice(possibilidades_minas)
+    validade = datetime.datetime.now() + datetime.timedelta(minutes=5)
+    hora_validade = validade.strftime("%H:%M")
+    mensagem_formatada = mensagem.format(possibilidade_mina_aleatoria, hora_validade)
+    bot.send_message(chat_id=channel_id, text=mensagem_formatada, parse_mode='Markdown')
+    time.sleep(120)
+
+    bot.send_message(chat_id=channel_id, text=texto5, parse_mode='Markdown')
+
+
+
+
+
+
+def check_and_send_signal():
+    current_time = datetime.datetime.now().strftime("%H:%M")
+    signal_times = [
+        "09:00", "14:00", "19:00"
+    ]
+
+    if current_time in signal_times:
+        send_signal()
+
+
+try:
+        check_and_send_signal()
+        # Wait for 1 minute before checking the time again
+        datetime.datetime.now() + datetime.timedelta(minutes=0)
+except Exception as e:
+        print(f"Error occurred: {str(e)}")
