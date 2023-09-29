bdd38baeafba471fb10701b3ea5de907f99a0cad
severino6464 <46749707+severino6464@users.noreply.github.com>
2023-09-29 17:22:27 -0300

    Atualização do arquivo

--- sala452.py
+++ sala452.py
@@ -24,8 +24,8 @@ mensagem = """
 
 ⚡ Intercalando
 
-[🔗 Fazer CADASTRO ✅](https://bit.ly/3LDnAAn)
-[🔗 Abrir FORTUNE TIGER](https://bit.ly/3LDnAAn)
+<a href="https://bit.ly/3LDnAAn">🔗 Fazer CADASTRO ✅</a>
+<a href="https://bit.ly/3LDnAAn">🔗 Abrir FORTUNE TIGER</a>
 
 """
 
@@ -44,7 +44,7 @@ validade = datetime.datetime.now() + datetime.timedelta(minutes=10)
 hora_validade = validade.strftime("%H:%M")
 mensagem_formatada = mensagem.format(hora_validade,n_jogadas, n_jogadas2)
 
-bot.send_message(chat_id=group_id, text=mensagem_formatada, parse_mode='Markdown')
+bot.send_message(chat_id=group_id, text=mensagem_formatada, parse_mode='HTML', disable_web_page_preview=True)
 
 time.sleep(600)  # Espera 10 minutos (600 segundos)
 
