e9e8d77723d022d67d4e8d5f03464d661ec5f8a1
severino6464 <46749707+severino6464@users.noreply.github.com>
2023-10-03 14:17:48 -0300

    Atualização do arquivo

---
+++ sala470.py
@@ -0,0 +1,90 @@
+import telebot
+import time
+import datetime
+import random
+
+CHAVE_API = "5597794728:AAGfwOg3RijfPrQ5S_Iw6NKAuYucNEdIsO8" # BOT FOX
+
+bot = telebot.TeleBot(CHAVE_API)
+
+group_id = '-1001960486501'
+
+
+def gerar_tres_opcoes_aleatorias(possibilidades):
+    tres_opcoes = random.sample(possibilidades, 3)
+    return ", ".join(tres_opcoes)
+
+links = [
+    "https://exemplo1.com",
+]
+
+
+possibilidades_minas = [
+"CACHORRO 🐕", "JACARÉ 🐊", "PAVÃO 🦚",
+"COELHO 🐇", "VACA 🐄", "PERU 🦃",
+"CERVO 🦌", "GATO 🐈", "COBRA 🐍",
+"BORBOLETA 🦋", "BOI 🐂", "CAMELO 🐪",
+"GALO 🐓", "PORCO 🐷", "URSO 🧸",
+"TIGRE 🐅", "GORILA 🦍", "ELEFANTE 🐘",
+"OVELHA 🐑", "ÁGUIA 🦅", "BUFFALO 🐃",
+"LEÃO 🦁", "CAVALO 🐎", "BODE 🐏",
+"AVESTRUZ 🦤", "CACHORRO 🐕", "COELHO 🐇",
+"JACARÉ 🐊", "PAVÃO 🦚", "VACA 🐄",
+"PERU 🦃", "CERVO 🦌", "GATO 🐈",
+"COBRA 🐍", "BORBOLETA 🦋", "BOI 🐂",
+"CAMELO 🐪", "GALO 🐓", "PORCO 🐷",
+"URSO 🧸", "TIGRE 🐅", "GORILA 🦍",
+"ELEFANTE 🐘", "OVELHA 🐑", "ÁGUIA 🦅",
+"BUFFALO 🐃", "LEÃO 🦁", "CAVALO 🐎",
+"BODE 🐏", "AVESTRUZ 🦤", "CACHORRO 🐕",
+"COELHO 🐇", "JACARÉ 🐊", "PAVÃO 🦚",
+"VACA 🐄", "PERU 🦃", "CERVO 🦌",
+"GATO 🐈", "COBRA 🐍", "BORBOLETA 🦋",
+"BOI 🐂", "CAMELO 🐪", "GALO 🐓",
+"PORCO 🐷", "URSO 🧸", "TIGRE 🐅",
+"GORILA 🦍", "ELEFANTE 🐘", "OVELHA 🐑",
+"ÁGUIA 🦅", "BUFFALO 🐃", "LEÃO 🦁",
+"CAVALO 🐎", "BODE 🐏", "AVESTRUZ 🦤"
+]
+
+
+
+texto4 = """
+🐝 Fique atento ao jogo 🦋
+
+🐞 Jogo do Bicho
+🔎 Estamos validando uma entrada
+
+[📱 Cadastre-se aqui](======)
+
+🚨O sinal só funciona apenas na plataforma acima!
+"""
+
+
+mensagem = """
+🔥 SINAL CONFIRMADO 🔥
+🎯 Jogar: TRINCA
+
+{}
+
+🔁 Fazer até 2 gales
+🔗 [Cadastre-se antes de Jogar!](=========)
+🖥️[Jogue Aqui](==========)
+⏱️ Válido até: {}
+"""
+
+
+
+
+print("======")
+
+possibilidade_mina_aleatoria = gerar_tres_opcoes_aleatorias(possibilidades_minas)
+link_aleatorio = random.choice(links)
+validade = datetime.datetime.now() + datetime.timedelta(minutes=5)
+hora_validade = validade.strftime("%H:%M")
+mensagem_formatada = mensagem.format(possibilidade_mina_aleatoria, hora_validade)
+mensagem_formatada = mensagem_formatada.replace("LINK_PLATAFORMA_CORRETA", link_aleatorio)
+mensagem_formatada = mensagem_formatada.replace("LINK_JOGO", link_aleatorio)
+
+bot.send_message(chat_id=group_id, text=mensagem_formatada, parse_mode='Markdown')
+time.sleep(480)
