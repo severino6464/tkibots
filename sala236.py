c0805f9972bab9d0a5ff2d71bb8b3a91e3655e2c
severino6464 <46749707+severino6464@users.noreply.github.com>
2023-10-03 17:25:50 -0300

    Atualização do arquivo

--- sala236.py
+++ sala236.py
@@ -1,124 +1,115 @@
-import time
-import requests
 import telebot
+import datetime
+import random
+import time
 
-ultimos_resultados = []
-check_resultados = []
-ultima_aposta = None  # Variável para rastrear a última aposta
 
-token = '5597794728:AAGfwOg3RijfPrQ5S_Iw6NKAuYucNEdIsO8' #fox bot
-chat_id = '-1001825767051'
+CHAVE_API = "5597794728:AAGfwOg3RijfPrQ5S_Iw6NKAuYucNEdIsO8" 
+
+bot = telebot.TeleBot(CHAVE_API)
+
+channel_id = '-1001825767051'  
+
+possibilidades_minas = [
+    "Apostar na duzia 1 e 3",
+    "Apostar na duzia 1 e 2",
+    "Apostar na duzia 2 e 3",
+]
+
 
 
 texto4 = """
-⚠️ ANALISANDO ENTRADAS⚠️
+ATENÇÃO VAMOS INICIAR 
 
-🎰 Roleta: Brasileira 
+FAÇA SEU CADASTRO 👇
+[Clique aqui](https://affiliates.nuts.bet/visit/?bta=36463&brand=nutsbet)
 
-🖥 Link de cadastro:[Clique aqui](https://affiliates.nuts.bet/visit/?bta=36463&brand=nutsbet)
+ENTRE NO JOGO👇🏻
+[Clique aqui](https://nuts.bet/live-casino/game/2177465)
+"""
+
+texto5 = """
+SESSÃO ENCERRADA!
 """
 
 
+mensagem = """
+💰 ENTRADA CONFIRMADA 💰
+🎰 Roleta: Brasileira
+Link: [Clique aqui](https://affiliates.nuts.bet/visit/?bta=36463&brand=nutsbet)
 
-# Função para puxar os dados da roleta
-def puxar_dados():
-    global ultimos_resultados
-    # Faz uma requisição GET para a URL específica
-    resposta = requests.get(url, headers=headers)
-    dic_resposta = resposta.json()
-    dados = dic_resposta['gameTables']
-    for i in dados:
-        roletas = i['gameTableId']
-        # Puxando os últimos resultados apenas da roleta brasileira
-        if '103910' in roletas:
-            chaves = i.keys()
-            if 'lastNumbers' in chaves:
-                ultimos_resultados = i['lastNumbers']
-    return ultimos_resultados
+{}
 
-# Função para determinar a cor do número (vermelho ou preto)
-def determinar_cor(numero):
-    vermelhos = ['1', '3', '5', '7', '9', '12', '14', '16', '18', '19', '21', '23', '25', '27', '30', '32', '34', '36']
-    if numero in vermelhos:
-        return "VERMELHO"
-    else:
-        return "PRETO"
-
-# Função para enviar mensagem para o Telegram
-def enviar_mensagem_telegram(mensagem):
-  
-    bot = telebot.TeleBot(token=token)
-    bot.send_message(chat_id=chat_id, text=mensagem,parse_mode='Markdown')
-
-url = "https://casino.betfair.com/api/tables-details"
-headers = {"cookie": "vid=8ab7daa7-57f7-4196-8285-943390594163"}
-
-
-ultimos_resultados.clear()
-puxar_dados()
-
-if ultimos_resultados != check_resultados:
-    check_resultados = ultimos_resultados
-    primeiro_numero = ultimos_resultados[0]
-    segundo_numero = ultimos_resultados[1]
-    terceiro_numero = ultimos_resultados[2]
-    quarto_numero = ultimos_resultados[3]
-    x1 = determinar_cor(primeiro_numero)
-    x2 = determinar_cor(segundo_numero)
-    x3 = determinar_cor(terceiro_numero)
-    x4 = determinar_cor(quarto_numero)
-
-    if x1 == x2 == x3 == x4:
-        cor_aposta = "🔴" if x1 == "PRETO" else "⚫"
-        ultima_aposta = x1
-        mensagem = f"""
-        💰 PADRÃO INDETIFICADO 💰
-        🚀 Entrada Confirmada
-        🎰 Roleta: Brasileira 
-
-        Entrar na cor: {cor_aposta}
-
-        🟢 Sempre Cobrir o Zero
-        🚨 Aplicar até 2 gales
-    
-        🎯[APOSTE AQUI](https://affiliates.nuts.bet/visit/?bta=36463&brand=nutsbet)
-        🎁 [Clique aqui](https://affiliates.nuts.bet/visit/?bta=36463&brand=nutsbet)
+👉 Cobrir o zero
+🔁 Fazer até 3 gales
+🔗 [Cadastre-se antes de Jogar!](https://affiliates.nuts.bet/visit/?bta=36463&brand=nutsbet)
+⏱️ Válido até: {}
 """
-        enviar_mensagem_telegram(mensagem)
-        print(mensagem)
-        time.sleep(45)
-           
-            
-    else:
-        mensagem = f"O primeiro número foi: {primeiro_numero}, e ele é da cor: {x1}\n"
-        mensagem += f"O segundo número foi: {segundo_numero}, e ele é da cor: {x2}\n"
-        mensagem += f"O terceiro número foi: {terceiro_numero}, e ele é da cor: {x3}\n"
-        mensagem += f"O quarto número foi: {quarto_numero}, e ele é da cor: {x4}"
 
-       
-    print(mensagem)
+def send_signal():
 
-if ultima_aposta is not None:
-        puxar_dados()
-        novo_resultado = determinar_cor(ultimos_resultados[0])
-        if novo_resultado == ultima_aposta:
-            mensagem_aposta_vencedora = f" RED ❌"
-            enviar_mensagem_telegram(mensagem_aposta_vencedora)
-            ultima_aposta = None  # Limpa a última aposta
-            time.sleep(10)
-             
-            bot = telebot.TeleBot(token=token)
-            bot.send_message(chat_id=chat_id, text=texto4, parse_mode='Markdown')
+    bot.send_message(chat_id=channel_id, text=texto4, parse_mode='Markdown')
+    time.sleep(120) 
 
-           
-        else:
-            mensagem_aposta_perdedora = f" GREENN ✅✅"
-            enviar_mensagem_telegram(mensagem_aposta_perdedora)
-            ultima_aposta = None  # Limpa a última aposta
-            time.sleep(10)
-              
-            bot = telebot.TeleBot(token=token)
-            bot.send_message(chat_id=chat_id, text=texto4, parse_mode='Markdown')
 
-   
-time.sleep(5)
+    possibilidade_mina_aleatoria = random.choice(possibilidades_minas)
+    validade = datetime.datetime.now() + datetime.timedelta(minutes=5)
+    hora_validade = validade.strftime("%H:%M")
+    mensagem_formatada = mensagem.format(possibilidade_mina_aleatoria, hora_validade)
+    bot.send_message(chat_id=channel_id, text=mensagem_formatada, parse_mode='Markdown')
+    time.sleep(600)
+
+    possibilidade_mina_aleatoria = random.choice(possibilidades_minas)
+    validade = datetime.datetime.now() + datetime.timedelta(minutes=5)
+    hora_validade = validade.strftime("%H:%M")
+    mensagem_formatada = mensagem.format(possibilidade_mina_aleatoria, hora_validade)
+    bot.send_message(chat_id=channel_id, text=mensagem_formatada, parse_mode='Markdown')
+    time.sleep(600)
+
+    possibilidade_mina_aleatoria = random.choice(possibilidades_minas)
+    validade = datetime.datetime.now() + datetime.timedelta(minutes=5)
+    hora_validade = validade.strftime("%H:%M")
+    mensagem_formatada = mensagem.format(possibilidade_mina_aleatoria, hora_validade)
+    bot.send_message(chat_id=channel_id, text=mensagem_formatada, parse_mode='Markdown')
+    time.sleep(600)
+
+    possibilidade_mina_aleatoria = random.choice(possibilidades_minas)
+    validade = datetime.datetime.now() + datetime.timedelta(minutes=5)
+    hora_validade = validade.strftime("%H:%M")
+    mensagem_formatada = mensagem.format(possibilidade_mina_aleatoria, hora_validade)
+    bot.send_message(chat_id=channel_id, text=mensagem_formatada, parse_mode='Markdown')
+    time.sleep(600)
+
+    possibilidade_mina_aleatoria = random.choice(possibilidades_minas)
+    validade = datetime.datetime.now() + datetime.timedelta(minutes=5)
+    hora_validade = validade.strftime("%H:%M")
+    mensagem_formatada = mensagem.format(possibilidade_mina_aleatoria, hora_validade)
+    bot.send_message(chat_id=channel_id, text=mensagem_formatada, parse_mode='Markdown')
+    time.sleep(600)
+
+    possibilidade_mina_aleatoria = random.choice(possibilidades_minas)
+    validade = datetime.datetime.now() + datetime.timedelta(minutes=5)
+    hora_validade = validade.strftime("%H:%M")
+    mensagem_formatada = mensagem.format(possibilidade_mina_aleatoria, hora_validade)
+    bot.send_message(chat_id=channel_id, text=mensagem_formatada, parse_mode='Markdown')
+    time.sleep(600)
+
+    bot.send_message(chat_id=channel_id, text=texto5, parse_mode='Markdown')
+
+
+def check_and_send_signal():
+    current_time = datetime.datetime.now().strftime("%H:%M")
+    signal_times = [
+        "10:00", "15:00", "21:00"
+    ]
+
+    if current_time in signal_times:
+        send_signal()
+
+
+try:
+        check_and_send_signal()
+        # Wait for 1 minute before checking the time again
+        datetime.datetime.now() + datetime.timedelta(minutes=0)
+except Exception as e:
+        print(f"Error occurred: {str(e)}")
