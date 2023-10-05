ffeea6bc59ed95983971db08e67a18d73de525db
severino6464 <46749707+severino6464@users.noreply.github.com>
2023-10-05 14:21:13 -0300

    Atualização do arquivo

--- sala452.py
+++ sala452.py
@@ -7,7 +7,7 @@ CHAVE_API = "5597794728:AAGfwOg3RijfPrQ5S_Iw6NKAuYucNEdIsO8" # BOT FOX
 
 bot = telebot.TeleBot(CHAVE_API)
 
-group_id = '-1001944503081'
+group_id = ''
 
 
 
@@ -25,7 +25,7 @@ mensagem = """
 ⚡ Intercalando
 
 <a href="https://www.jogomanga.com/c-BZPxboKv?lang=pt">🔗 Fazer CADASTRO ✅</a>
-<a href="https://www.jogomanga.com/c-BZPxboKv?lang=pt">🔗 Abrir FORTUNE TIGER</a>
+<a href="https://www.jogomanga.com/c-BZPxboKv?lang=pt">🔗 Abrir Lucky Neko</a>
 
 """
 
