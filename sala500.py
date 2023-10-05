4b34a4b43cfd0efedaa81c3a371ceb92d150d4a9
severino6464 <46749707+severino6464@users.noreply.github.com>
2023-10-05 19:44:38 -0300

    Atualização do arquivo

--- sala500.py
+++ sala500.py
@@ -14,7 +14,7 @@ texto4 = """
 🚨🍀 ATENÇÃO VAMOS INICIAR OS SINAIS DO LUCKY NEKO 🍀🚨
 
 FAÇA SEU CADASTRO 👇
-<a href="https://go.aff.br4-partners.com/w1pjlefq">🔗 Fazer CADASTRO </a>
+<a href="https://www.jogomanga.com/c-BZPxboKv?lang=pt">🔗 Fazer CADASTRO </a>
 
 """
 
@@ -29,8 +29,8 @@ mensagem = """
 
 ⚡ Intercalando
 
-<a href="https://go.aff.br4-partners.com/w1pjlefq">🔗 Fazer CADASTRO ✅</a>
-<a href="https://go.aff.br4-partners.com/w1pjlefq">🔗 Abrir LUCKY NEKO</a>
+<a href="https://www.jogomanga.com/c-BZPxboKv?lang=pt">🔗 Fazer CADASTRO ✅</a>
+<a href="https://www.jogomanga.com/c-BZPxboKv?lang=pt">🔗 Abrir LUCKY NEKO</a>
 
 """
 
