80134dd6f9af5b20214882f5053a916cca3a57ce
severino6464 <46749707+severino6464@users.noreply.github.com>
2023-10-05 18:25:18 -0300

    Atualização do arquivo

--- sala498.py
+++ sala498.py
@@ -46,10 +46,10 @@ mensagem = """
 🎮: Tentativas: 2
 Jogar com 2 a 3 minas
 
-📲: Plataforma correta: [Clique aqui](https://go.aff.bullsbetaffiliate.com/p79vpxd3)
-👉🏻: Link do jogo: [Mines](https://go.aff.bullsbetaffiliate.com/p79vpxd3)
 ⏱️ Válido até: {}
 
+<a href="https://go.aff.bullsbetaffiliate.com/p79vpxd3">📲: Plataforma correta</a>
+<a href="https://nuts.bet/casino/game/2172765">👉🏻: Link do jogo</a>
 """
 
 
@@ -67,8 +67,8 @@ hora_validade = validade.strftime("%H:%M")
 mensagem_formatada = mensagem.format(possibilidade_mina_aleatoria, hora_validade)
 mensagem_formatada = mensagem_formatada.replace("LINK_PLATAFORMA_CORRETA", link_aleatorio)
 mensagem_formatada = mensagem_formatada.replace("LINK_JOGO", link_aleatorio)
-bot.send_message(chat_id=group_id, text=mensagem_formatada, parse_mode='Markdown')
+bot.send_message(chat_id=group_id, text=mensagem_formatada, parse_mode='HTML', disable_web_page_preview=True)
    
 time.sleep(120)
-bot.send_message(chat_id=group_id, text=text2 ,parse_mode='Markdown')
+bot.send_message(chat_id=group_id, text=text2 ,parse_mode='HTML', disable_web_page_preview=True)
 time.sleep(300)  # Espera 5 minutos (300 segundos)
