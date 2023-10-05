9f99fa69cf936d27988c29d85999565259ee8ba7
severino6464 <46749707+severino6464@users.noreply.github.com>
2023-10-05 19:44:59 -0300

    Atualização do arquivo

--- sala502.py
+++ sala502.py
@@ -36,7 +36,7 @@ possibilidades_minas = [
 texto4 = """
 ⭐️💣 <b>ATENÇÃO VAMOS INICIAR A SESSÃO DO MINES</b> ⭐️💣
 
-<a href="https://go.aff.br4-partners.com/w1pjlefq">📱 CADASTRE-SE AQUI</a>
+<a href="https://www.jogomanga.com/c-BZPxboKv?lang=pt">📱 CADASTRE-SE AQUI</a>
 """
 
 texto5 = """
@@ -60,8 +60,8 @@ Jogar com 2 a 3 minas
 
 ⏱️ Válido até: {}
 
-<a href="https://go.aff.br4-partners.com/w1pjlefq">📲 Plataforma correta</a>
-<a href="https://go.aff.br4-partners.com/w1pjlefq">👉🏻 Link do jogo</a>
+<a href="https://www.jogomanga.com/c-BZPxboKv?lang=pt">📲 Plataforma correta</a>
+<a href="https://www.jogomanga.com/c-BZPxboKv?lang=pt">👉🏻 Link do jogo</a>
 """
 
 def send_signal():
