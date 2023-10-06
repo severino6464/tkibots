541e07b1b57eb412fec9e6e5cc9d87889cccaf77
severino6464 <46749707+severino6464@users.noreply.github.com>
2023-10-06 14:24:04 -0300

    Atualização do arquivo

--- sala450.py
+++ sala450.py
@@ -1,13 +1,14 @@
 import telebot
-import time
 import datetime
 import random
+import time
+
 
 CHAVE_API = "5597794728:AAGfwOg3RijfPrQ5S_Iw6NKAuYucNEdIsO8" 
 
 bot = telebot.TeleBot(CHAVE_API)
 
-group_id = '-1001869121732'
+channel_id = '-1001869121732'  
 
 possibilidades_minas = [
     "💣⭐️⭐️💣💣\n💣💣⭐️💣💣\n💣💣💣💣💣\n💣💣⭐️⭐️⭐️\n💣💣⭐️💣💣",
@@ -31,44 +32,84 @@ possibilidades_minas = [
     "⭐️💣💣💣💣\n💣💣💣💣💣\n⭐️💣💣💣💣\n💣⭐️💣⭐️💣\n💣⭐️💣⭐️💣"
 ]
 
-links = [
-    "https://exemplo1.com",
-   
-]
 
+texto4 = """
+⭐️💣 <b>ATENÇÃO VAMOS INICIAR A SESSÃO DO MINES</b> ⭐️💣
+
+<a href="https://www.jogomanga.com/c-BZPxboKv?lang=pt">📱 CADASTRE-SE AQUI</a>
+"""
+
+texto5 = """
+✅🤑 <b>BATEUUUU</b> 🤑✅
+"""
+
+texto6 = """
+⭐️💣 <b>SESSÃO ENCERRADA!</b> ⭐️💣
+"""
 
 
 mensagem = """
-🎲 Entrada confirmada 🎲
+🎲 <b>Entrada confirmada</b> 🎲
+⭐️💣 <b>MINES</b> ⭐️💣
 🥇: Entrada 
 
 {}
+
 🎮: Tentativas: 2
 Jogar com 2 a 3 minas
 
 ⏱️ Válido até: {}
 
-<a href="https://www.jogomanga.com/c-BZPxboKv?lang=pt">📲: Plataforma correta</a>
-<a href="https://www.jogomanga.com/c-BZPxboKv?lang=pt">👉🏻: Link do jogo</a>
+<a href="https://www.jogomanga.com/c-BZPxboKv?lang=pt">📲 Plataforma correta</a>
+<a href="https://www.jogomanga.com/c-BZPxboKv?lang=pt">👉🏻 Link do jogo</a>
 """
 
+def send_signal():
+    bot.send_message(chat_id=channel_id, text=texto4, parse_mode='HTML', disable_web_page_preview=True)
+    time.sleep(300) 
+    possibilidade_mina_aleatoria = random.choice(possibilidades_minas)
+    validade = datetime.datetime.now() + datetime.timedelta(minutes=10)
+    hora_validade = validade.strftime("%H:%M")
+    mensagem_formatada = mensagem.format(possibilidade_mina_aleatoria, hora_validade)
+    bot.send_message(chat_id=channel_id, text=mensagem_formatada, parse_mode='HTML', disable_web_page_preview=True)
+    time.sleep(480)
+    bot.send_message(chat_id=channel_id, text=texto5, parse_mode='HTML', disable_web_page_preview=True)
+    time.sleep(420)
 
-text2 = """
-🔷🔹 Entrada Finalizada 🔹🔷
-     ✅✅ GRENN! ✅✅
-  """
+    possibilidade_mina_aleatoria = random.choice(possibilidades_minas)
+    validade = datetime.datetime.now() + datetime.timedelta(minutes=10)
+    hora_validade = validade.strftime("%H:%M")
+    mensagem_formatada = mensagem.format(possibilidade_mina_aleatoria, hora_validade)
+    bot.send_message(chat_id=channel_id, text=mensagem_formatada, parse_mode='HTML', disable_web_page_preview=True)
+    time.sleep(480)
+    bot.send_message(chat_id=channel_id, text=texto5, parse_mode='HTML', disable_web_page_preview=True)
+    time.sleep(420)
 
-print("=====")
+    possibilidade_mina_aleatoria = random.choice(possibilidades_minas)
+    validade = datetime.datetime.now() + datetime.timedelta(minutes=10)
+    hora_validade = validade.strftime("%H:%M")
+    mensagem_formatada = mensagem.format(possibilidade_mina_aleatoria, hora_validade)
+    bot.send_message(chat_id=channel_id, text=mensagem_formatada, parse_mode='HTML', disable_web_page_preview=True)
+    time.sleep(480)
+    bot.send_message(chat_id=channel_id, text=texto5, parse_mode='HTML', disable_web_page_preview=True)
+    time.sleep(30)
 
-possibilidade_mina_aleatoria = random.choice(possibilidades_minas)
-link_aleatorio = random.choice(links)
-validade = datetime.datetime.now() + datetime.timedelta(minutes=5)
-hora_validade = validade.strftime("%H:%M")
-mensagem_formatada = mensagem.format(possibilidade_mina_aleatoria, hora_validade)
-mensagem_formatada = mensagem_formatada.replace("LINK_PLATAFORMA_CORRETA", link_aleatorio)
-mensagem_formatada = mensagem_formatada.replace("LINK_JOGO", link_aleatorio)
-bot.send_message(chat_id=group_id, text=mensagem_formatada, parse_mode='HTML', disable_web_page_preview=True)
-   
-time.sleep(120)
-bot.send_message(chat_id=group_id, text=text2 ,parse_mode='HTML', disable_web_page_preview=True)
-time.sleep(300)  # Espera 5 minutos (300 segundos)
+    bot.send_message(chat_id=channel_id, text=texto6, parse_mode='HTML', disable_web_page_preview=True)
+
+
+def check_and_send_signal():
+    current_time = datetime.datetime.now().strftime("%H:%M")
+    signal_times = [
+         "01:00", "04:00", "07:00", "10:00", "13:00", "16:00", "19:00", "22:00"
+    ]
+
+    if current_time in signal_times:
+        send_signal()
+
+
+try:
+    check_and_send_signal()
+    # Wait for 1 minute before checking the time again
+    datetime.datetime.now() + datetime.timedelta(minutes=0)
+except Exception as e:
+    print(f"Error occurred: {str(e)}")
