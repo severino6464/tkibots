b23ba295144d93696750860a05c8ad47fa8fdc60
severino6464 <46749707+severino6464@users.noreply.github.com>
2023-10-09 12:40:53 -0300

    Atualização do arquivo

--- sala499.py
+++ sala499.py
@@ -56,7 +56,7 @@ bot.send_message(chat_id=group_id, text=texto4, parse_mode='HTML', disable_web_p
 time.sleep(120)
 
 bot.send_message(chat_id=group_id, text=mensagem_formatada, parse_mode='HTML', disable_web_page_preview=True)
-time.sleep(1200)
+time.sleep(1080)
 
 bot.send_message(chat_id=group_id, text=texto5, parse_mode='HTML', disable_web_page_preview=True)
 time.sleep(600)
