fcf8473f2744d415150c4957810be39f777ed187
severino6464 <46749707+severino6464@users.noreply.github.com>
2023-10-14 12:57:59 -0300

    Atualização do arquivo

---
+++ sala575.py
@@ -0,0 +1,122 @@
+import telebot
+import datetime
+import random
+import time
+
+
+CHAVE_API = "5597794728:AAGfwOg3RijfPrQ5S_Iw6NKAuYucNEdIsO8" 
+
+bot = telebot.TeleBot(CHAVE_API)
+
+channel_id = '-1001589531131'  
+
+sticker_file_id = 'CAACAgEAAxkBAAMCZSbmh4EopfmSJgx8Z8sDxkeWf1UAAvwAAzgOghFAju2fQymOBzAE'
+
+texto4 = """
+🐯💎<b>ATENÇÃO SESSÃO DO TIGER VAI INICIAR EM 2 MINUTOS</b>💎🐯
+
+<a href="https://affiliates.nuts.bet/visit/?bta=37719&brand=nutsbet">📱 CADASTRE-SE AQUI</a>
+"""
+
+texto5 = """
+<b>✅🤑BATEUUUU🤑✅</b>
+"""
+
+texto6= """
+🐯 <b>SESSÃO DO FORTUNE TIGER ENCERRADA!</b> 🐯
+"""
+
+mensagem = """
+🚨 <b>ENTRADA CONFIRMADA</b> 🚨
+
+🐯 FORTUNE TIGER ☘️
+🔥 Jogadas: {}
+⏰ Válido até: {}
+
+🌪 Faça no máximo {} jogadas!
+
+<a href="https://affiliates.nuts.bet/visit/?bta=37719&brand=nutsbet">📱 CADASTRE-SE AQUI</a>
+
+<a href="https://affiliates.nuts.bet/visit/?bta=37719&brand=nutsbet">📱 ABRIR FORTUNE TIGER🐯☘️</a>
+"""
+
+def send_signal():
+
+    bot.send_message(chat_id=channel_id, text=texto4, parse_mode='HTML', disable_web_page_preview=True)
+    time.sleep(120) 
+    n_jogadas = random.randint(6, 20)
+    n_jogadas2 = random.randint(4, 20)
+    validade = datetime.datetime.now() + datetime.timedelta(minutes=4)
+    hora_validade = validade.strftime("%H:%M")
+    mensagem_formatada = mensagem.format(n_jogadas, hora_validade, n_jogadas2)
+    bot.send_message(chat_id=channel_id, text=mensagem_formatada, parse_mode='HTML', disable_web_page_preview=True)
+    time.sleep(180)
+    bot.send_message(chat_id=channel_id, text=texto5, parse_mode='HTML', disable_web_page_preview=True)
+    bot.send_sticker(chat_id=channel_id, sticker=sticker_file_id)
+    time.sleep(120)
+
+    n_jogadas = random.randint(6, 20)
+    n_jogadas2 = random.randint(4, 20)
+    validade = datetime.datetime.now() + datetime.timedelta(minutes=4)
+    hora_validade = validade.strftime("%H:%M")
+    mensagem_formatada = mensagem.format(n_jogadas, hora_validade, n_jogadas2)
+    bot.send_message(chat_id=channel_id, text=mensagem_formatada, parse_mode='HTML', disable_web_page_preview=True)
+    time.sleep(180)
+    bot.send_message(chat_id=channel_id, text=texto5, parse_mode='HTML', disable_web_page_preview=True)
+    bot.send_sticker(chat_id=channel_id, sticker=sticker_file_id)
+    time.sleep(120)
+
+    n_jogadas = random.randint(6, 20)
+    n_jogadas2 = random.randint(4, 20)
+    validade = datetime.datetime.now() + datetime.timedelta(minutes=4)
+    hora_validade = validade.strftime("%H:%M")
+    mensagem_formatada = mensagem.format(n_jogadas, hora_validade, n_jogadas2)
+    bot.send_message(chat_id=channel_id, text=mensagem_formatada, parse_mode='HTML', disable_web_page_preview=True)
+    time.sleep(180)
+    bot.send_message(chat_id=channel_id, text=texto5, parse_mode='HTML', disable_web_page_preview=True)
+    bot.send_sticker(chat_id=channel_id, sticker=sticker_file_id)
+    time.sleep(120)
+
+    n_jogadas = random.randint(6, 20)
+    n_jogadas2 = random.randint(4, 20)
+    validade = datetime.datetime.now() + datetime.timedelta(minutes=4)
+    hora_validade = validade.strftime("%H:%M")
+    mensagem_formatada = mensagem.format(n_jogadas, hora_validade, n_jogadas2)
+    bot.send_message(chat_id=channel_id, text=mensagem_formatada, parse_mode='HTML', disable_web_page_preview=True)
+    time.sleep(180)
+    bot.send_message(chat_id=channel_id, text=texto5, parse_mode='HTML', disable_web_page_preview=True)
+    bot.send_sticker(chat_id=channel_id, sticker=sticker_file_id)
+    time.sleep(120)
+
+    n_jogadas = random.randint(6, 20)
+    n_jogadas2 = random.randint(4, 20)
+    validade = datetime.datetime.now() + datetime.timedelta(minutes=4)
+    hora_validade = validade.strftime("%H:%M")
+    mensagem_formatada = mensagem.format(n_jogadas, hora_validade, n_jogadas2)
+    bot.send_message(chat_id=channel_id, text=mensagem_formatada, parse_mode='HTML', disable_web_page_preview=True)
+    time.sleep(180)
+    bot.send_message(chat_id=channel_id, text=texto5, parse_mode='HTML', disable_web_page_preview=True)
+    bot.send_sticker(chat_id=channel_id, sticker=sticker_file_id)
+    time.sleep(120)
+
+    bot.send_message(chat_id=channel_id, text=texto6, parse_mode='HTML', disable_web_page_preview=True)
+
+    
+
+
+def check_and_send_signal():
+    current_time = datetime.datetime.now().strftime("%H:%M")
+    signal_times = [
+       "09:00", "13:00", "20:00"
+    ]
+
+    if current_time in signal_times:
+        send_signal()
+
+
+try:
+        check_and_send_signal()
+        # Wait for 1 minute before checking the time again
+        datetime.datetime.now() + datetime.timedelta(minutes=0)
+except Exception as e:
+        print(f"Error occurred: {str(e)}")
