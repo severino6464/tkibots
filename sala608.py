d5a4c2a30ac3db6b224468e3a62df3ca1e9c472e
severino6464 <46749707+severino6464@users.noreply.github.com>
2023-10-18 22:46:39 -0300

    Atualização do arquivo

--- sala608.py
+++ sala608.py
@@ -7,7 +7,7 @@ CHAVE_API = "5597794728:AAGfwOg3RijfPrQ5S_Iw6NKAuYucNEdIsO8" # BOT FOX
 
 bot = telebot.TeleBot(CHAVE_API)
 
-group_id = '-1002067392980'
+group_id = '-1001915315846'
 
 sticker_file_id = 'CAACAgEAAxkBAAMCZSbmh4EopfmSJgx8Z8sDxkeWf1UAAvwAAzgOghFAju2fQymOBzAE'
 
