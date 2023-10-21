0da3abb4ae088a3b679bd0a444d1a648df537c4a
severino6464 <46749707+severino6464@users.noreply.github.com>
2023-10-20 23:17:26 -0300

    Atualização do arquivo

--- codigoprincipal.py
+++ codigoprincipal.py
@@ -1,28 +1,113 @@
-import subprocess
-import os
-import threading
+import telebot
+import datetime
+import random
+import time
+import requests
 
-# Função para executar um código de sala em loop
-def executar_sala(sala_codigo):
-    while True:
-        subprocess.run(["python3", os.path.join(base_path, sala_codigo)])
+CHAVE_API = "5597794728:AAGfwOg3RijfPrQ5S_Iw6NKAuYucNEdIsO8"  # Substitua pelo seu token de bot
+bot = telebot.TeleBot(CHAVE_API)
 
-# Número total de salas
-num_salas = 800
+channel_id = '-1001896242506'  # Substitua pelo ID do seu canal
 
-# Caminho para a pasta onde o código está sendo executado
-base_path = os.path.dirname(os.path.abspath(__file__))
+possibilidades_minas = [
+    "Apostar na 1° e 2° coluna",
+    "Apostar na 1° e 3° coluna",
+    "Apostar na 2° e 3° coluna",
+]
 
-# Lista para armazenar as threads
-threads = []
+url = "https://casino.betfair.com/api/tables-details"
+headers = {"cookie": "vid=8ab7daa7-57f7-4196-8285-943390594163"}
 
-# Loop para criar threads para cada sala e iniciar a execução em loop
-for sala_numero in range(1, num_salas + 1):
-    sala_codigo = f"sala{sala_numero}.py"
-    thread = threading.Thread(target=executar_sala, args=(sala_codigo,))
-    threads.append(thread)
-    thread.start()
+def puxar_dados():
+    resposta = requests.get(url, headers=headers)
+    dic_resposta = resposta.json()
+    dados = dic_resposta['gameTables']
+    for i in dados:
+        roletas = i['gameTableId']
+        if '103910' in roletas:
+            chaves = i.keys()
+            if 'lastNumbers' in chaves:
+                ultimos_resultados = i['lastNumbers']
+                return ultimos_resultados
 
-# Aguardar todas as threads terminarem (que não acontecerá nesse caso)
-for thread in threads:
-    thread.join()
+texto4 = """
+⚠️ <b>ATENÇÃO VAMOS INICIAR</b> ⚠️
+
+<a href="https://affiliates.nuts.bet/visit/?bta=35722&brand=nutsbet"><b>💸 Clique aqui para abrir a corretora!</b></a>
+"""
+
+mensagem = """
+🎯 <b>Entrada Confirmada</b> 🎯
+
+🖥️ Roleta: ROLETA BRASILEIRA 🇧🇷
+🔁 Até duas proteções - Cobrir o zero!
+
+🔥 Entrar: {}
+
+🧨 Último número: {}
+⏱️ Válido até: {}
+
+<a href="https://affiliates.nuts.bet/visit/?bta=35722&brand=nutsbet"><b>💸 Clique aqui para abrir a corretora!</b></a>
+"""
+
+# Lista para armazenar os sinais enviados
+sinais_enviados = []
+
+def enviar_sinal(resultado, contagem_verde, contagem_vermelho):
+    ultimos_resultados = puxar_dados()
+    if ultimos_resultados:
+        ultimo_numero = ultimos_resultados[0]
+        possibilidade_mina_aleatoria = random.choice(possibilidades_minas)
+        mensagem_formatada = mensagem.format(possibilidade_mina_aleatoria, ultimo_numero, resultado['hora'])
+        bot.send_message(chat_id=channel_id, text=mensagem_formatada, parse_mode='HTML', disable_web_page_preview=True)
+        sinais_enviados.append(resultado)
+        if resultado['resultado'] == 'GREEN ✅':
+            contagem_verde += 1
+        elif resultado['resultado'] == 'RED ❌':
+            contagem_vermelho += 1
+        return contagem_verde, contagem_vermelho
+
+def gerar_relatorio(contagem_verde, contagem_vermelho):
+    data_atual = datetime.datetime.now().strftime("%d/%m/%Y")
+    relatorio = f"📔 RELATÓRIO DE OPERAÇÕES ({data_atual})\n\n"
+
+    for resultado in sinais_enviados:
+        relatorio += f"{resultado['hora']} - {resultado['resultado']}\n"
+
+    placar = f"\nPlacar: ✅ {contagem_verde} x ❌ {contagem_vermelho}\n"
+    relatorio += placar
+
+    return relatorio
+
+def enviar_relatorio(contagem_verde, contagem_vermelho):
+    relatorio = gerar_relatorio(contagem_verde, contagem_vermelho)
+    bot.send_message(chat_id=channel_id, text=relatorio, parse_mode='HTML', disable_web_page_preview=True)
+
+def enviar_sinais_e_relatorio():
+    contagem_verde = 0
+    contagem_vermelho = 0
+
+    # Enviar o texto4 antes dos sinais
+    bot.send_message(chat_id=channel_id, text=texto4, parse_mode='HTML', disable_web_page_preview=True)
+
+    # Aguardar 5 minutos antes de iniciar os sinais
+    time.sleep(5 * 60)
+
+    for _ in range(6):
+        resultado = {'hora': datetime.datetime.now().strftime("%H:%M"), 'resultado': 'GREEN ✅' if contagem_verde < 5 else 'RED ❌'}
+        
+        contagem_verde, contagem_vermelho = enviar_sinal(resultado, contagem_verde, contagem_vermelho)
+        
+        time.sleep(10 * 60)
+
+    enviar_relatorio(contagem_verde, contagem_vermelho)
+
+def check_and_send_signal():
+    current_time = datetime.datetime.now().strftime("%H:%M")
+    signal_times = ["09:00", "14:00"]
+
+    if current_time in signal_times:
+        enviar_sinais_e_relatorio()
+
+# Chame a função check_and_send_signal a partir do seu código principal sempre que necessário
+check_and_send_signal()
