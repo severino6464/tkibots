a38e3e94dbe151d2a9b5b0049d0ea10e9c611f2b
severino6464 <46749707+severino6464@users.noreply.github.com>
2023-10-21 13:18:36 -0300

    Atualização do arquivo

--- sala644.py
+++ sala644.py
@@ -104,7 +104,7 @@ def enviar_sinais_e_relatorio():
 
 def check_and_send_signal():
     current_time = datetime.datetime.now().strftime("%H:%M")
-    signal_times = ["10:30", "15:30","19:00""13:16"]
+    signal_times = ["10:30", "15:30","19:00","13:18"]
 
     if current_time in signal_times:
         enviar_sinais_e_relatorio()
