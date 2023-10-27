0b4adbff1f7b15753ffa1f46188ad95009011851
severino tiburcio (TKI TECNOLOGIA) <46749707+severino6464@users.noreply.github.com>
2023-10-27 13:34:53 -0300

    Atualização do arquivo

--- sala681.py
+++ sala681.py
@@ -7,7 +7,7 @@ CHAVE_API = "5597794728:AAGfwOg3RijfPrQ5S_Iw6NKAuYucNEdIsO8"
 
 bot = telebot.TeleBot(CHAVE_API)
 
-group_id = '-1002087161776'
+group_id = '-1002140872361'
 
 sticker_file_id = 'CAACAgIAAxkBAAMmZSb_ngXS-jrJPaIDkQxNkCtYOQQAAtgLAAJYD5hKNPj69b5xWK8wBA'
 
