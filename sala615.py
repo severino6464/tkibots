3073257ec677c1157f94b95a498eb93456b71931
severino tiburcio (TKI TECNOLOGIA) <46749707+severino6464@users.noreply.github.com>
2023-10-28 11:20:41 -0300

    Atualização do arquivo

--- sala615.py
+++ sala615.py
@@ -21,8 +21,6 @@ possibilidades_minas = [
  
 ]
 
-
-
 texto4 = """
 ⚠️ Fique atento ao jogo ⚠️
 
@@ -32,14 +30,11 @@ texto4 = """
 🖥 Link de cadastro:[Clique aqui](https://affiliates.nuts.bet/visit/?bta=37072&nci=5344)
 """
 
-
 texto5 = """
 🔷🔹 Entrada Finalizada 🔹🔷
      ✅✅ GRENN! ✅✅
  
 """
-
-
 
 mensagem = """
 ✅ Entrada Confirmada 
@@ -55,26 +50,136 @@ mensagem = """
 📲: Plataforma correta: [Clique aqui](https://affiliates.nuts.bet/visit/?bta=37072&nci=5344))
 """
 
+texto6 = """
+🚨🚨 HORARIOS DE FUNCIONAMENTO MÉTODO AVIATOR🚨🚨
+
+
+✅ Ai boca atenção que vai começar a sessão do método ✅
+✅ sessão todos os dias ✅
+
+⏰ 09:00 - 9:40 
+
+⏰ 13:00 - 13:40
+
+⏰ 19:00- 19:40
+
+⏰ 22:00 - 22:40
+
+
+MÉTODO AVIATOR:
+https://t.me/+FfjehVUGzWY4NTdh
+
+MÉTODO PENALTY: 
+https://t.me/+vguYFi-7FFw0MDJh
 
 
 
+📌 Vídeo ensinando como funciona fixado no topo do GRUPO✅
+"""
 
-print("=======")
+def send_signal():
 
-bot.send_message(chat_id=group_id, text=texto4, parse_mode='Markdown')
-time.sleep(120) 
+    bot.send_message(chat_id=group_id, text=texto4, parse_mode='Markdown')
+    time.sleep(120) 
+    possibilidade_mina_aleatoria = random.choice(possibilidades_minas)
+    validade = datetime.datetime.now() + datetime.timedelta(minutes=1)
+    hora_validade = validade.strftime("%H:%M")
+    mensagem_formatada = mensagem.format(possibilidade_mina_aleatoria, hora_validade)
+    bot.send_message(chat_id=group_id, text=mensagem_formatada, parse_mode='Markdown')
+    time.sleep(60)  # Espera 5 minutos (300 segundos)
+    bot.send_message(chat_id=group_id, text=texto5, parse_mode='Markdown')
+    time.sleep(120) 
+
+    bot.send_message(chat_id=group_id, text=texto4, parse_mode='Markdown')
+    time.sleep(120) 
+    possibilidade_mina_aleatoria = random.choice(possibilidades_minas)
+    validade = datetime.datetime.now() + datetime.timedelta(minutes=1)
+    hora_validade = validade.strftime("%H:%M")
+    mensagem_formatada = mensagem.format(possibilidade_mina_aleatoria, hora_validade)
+    bot.send_message(chat_id=group_id, text=mensagem_formatada, parse_mode='Markdown')
+    time.sleep(60)  # Espera 5 minutos (300 segundos)
+    bot.send_message(chat_id=group_id, text=texto5, parse_mode='Markdown')
+    time.sleep(120) 
+
+    bot.send_message(chat_id=group_id, text=texto4, parse_mode='Markdown')
+    time.sleep(120) 
+    possibilidade_mina_aleatoria = random.choice(possibilidades_minas)
+    validade = datetime.datetime.now() + datetime.timedelta(minutes=1)
+    hora_validade = validade.strftime("%H:%M")
+    mensagem_formatada = mensagem.format(possibilidade_mina_aleatoria, hora_validade)
+    bot.send_message(chat_id=group_id, text=mensagem_formatada, parse_mode='Markdown')
+    time.sleep(60)  # Espera 5 minutos (300 segundos)
+    bot.send_message(chat_id=group_id, text=texto5, parse_mode='Markdown')
+    time.sleep(120) 
+
+    bot.send_message(chat_id=group_id, text=texto4, parse_mode='Markdown')
+    time.sleep(120) 
+    possibilidade_mina_aleatoria = random.choice(possibilidades_minas)
+    validade = datetime.datetime.now() + datetime.timedelta(minutes=1)
+    hora_validade = validade.strftime("%H:%M")
+    mensagem_formatada = mensagem.format(possibilidade_mina_aleatoria, hora_validade)
+    bot.send_message(chat_id=group_id, text=mensagem_formatada, parse_mode='Markdown')
+    time.sleep(60)  # Espera 5 minutos (300 segundos)
+    bot.send_message(chat_id=group_id, text=texto5, parse_mode='Markdown')
+    time.sleep(120) 
+
+    bot.send_message(chat_id=group_id, text=texto4, parse_mode='Markdown')
+    time.sleep(120) 
+    possibilidade_mina_aleatoria = random.choice(possibilidades_minas)
+    validade = datetime.datetime.now() + datetime.timedelta(minutes=1)
+    hora_validade = validade.strftime("%H:%M")
+    mensagem_formatada = mensagem.format(possibilidade_mina_aleatoria, hora_validade)
+    bot.send_message(chat_id=group_id, text=mensagem_formatada, parse_mode='Markdown')
+    time.sleep(60)  # Espera 5 minutos (300 segundos)
+    bot.send_message(chat_id=group_id, text=texto5, parse_mode='Markdown')
+    time.sleep(120) 
+
+    bot.send_message(chat_id=group_id, text=texto4, parse_mode='Markdown')
+    time.sleep(120) 
+    possibilidade_mina_aleatoria = random.choice(possibilidades_minas)
+    validade = datetime.datetime.now() + datetime.timedelta(minutes=1)
+    hora_validade = validade.strftime("%H:%M")
+    mensagem_formatada = mensagem.format(possibilidade_mina_aleatoria, hora_validade)
+    bot.send_message(chat_id=group_id, text=mensagem_formatada, parse_mode='Markdown')
+    time.sleep(60)  # Espera 5 minutos (300 segundos)
+    bot.send_message(chat_id=group_id, text=texto5, parse_mode='Markdown')
+    time.sleep(120) 
+
+    bot.send_message(chat_id=group_id, text=texto4, parse_mode='Markdown')
+    time.sleep(120) 
+    possibilidade_mina_aleatoria = random.choice(possibilidades_minas)
+    validade = datetime.datetime.now() + datetime.timedelta(minutes=1)
+    hora_validade = validade.strftime("%H:%M")
+    mensagem_formatada = mensagem.format(possibilidade_mina_aleatoria, hora_validade)
+    bot.send_message(chat_id=group_id, text=mensagem_formatada, parse_mode='Markdown')
+    time.sleep(60)  # Espera 5 minutos (300 segundos)
+    bot.send_message(chat_id=group_id, text=texto5, parse_mode='Markdown')
+    time.sleep(120) 
+
+    bot.send_message(chat_id=group_id, text=texto4, parse_mode='Markdown')
+    time.sleep(120) 
+    possibilidade_mina_aleatoria = random.choice(possibilidades_minas)
+    validade = datetime.datetime.now() + datetime.timedelta(minutes=1)
+    hora_validade = validade.strftime("%H:%M")
+    mensagem_formatada = mensagem.format(possibilidade_mina_aleatoria, hora_validade)
+    bot.send_message(chat_id=group_id, text=mensagem_formatada, parse_mode='Markdown')
+    time.sleep(60)  # Espera 5 minutos (300 segundos)
+    bot.send_message(chat_id=group_id, text=texto5, parse_mode='Markdown')
+    time.sleep(120) 
+    bot.send_message(chat_id=group_id, text=texto6, parse_mode='Markdown')
+
+def check_and_send_signal():
+    current_time = datetime.datetime.now().strftime("%H:%M")
+    signal_times = [
+        "09:00", "13:00", "19:00", "22:00"
+    ]
+
+    if current_time in signal_times:
+        send_signal()
 
 
-
-  
-possibilidade_mina_aleatoria = random.choice(possibilidades_minas)
-validade = datetime.datetime.now() + datetime.timedelta(minutes=1)
-hora_validade = validade.strftime("%H:%M")
-mensagem_formatada = mensagem.format(possibilidade_mina_aleatoria, hora_validade)
-
-bot.send_message(chat_id=group_id, text=mensagem_formatada, parse_mode='Markdown')
-
-time.sleep(60)  # Espera 5 minutos (300 segundos)
-
-bot.send_message(chat_id=group_id, text=texto5, parse_mode='Markdown')
-time.sleep(120) 
+try:
+        check_and_send_signal()
+        datetime.datetime.now() + datetime.timedelta(minutes=0)
+except Exception as e:
+        print(f"Error occurred: {str(e)}")
