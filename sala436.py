0c50511ed5dc8ce4ed8db5893c2fabb4b985de53
severino tiburcio (TKI TECNOLOGIA) <46749707+severino6464@users.noreply.github.com>
2023-10-30 13:47:27 -0300

    Atualização do arquivo

--- sala436.py
+++ sala436.py
@@ -1,51 +1,126 @@
-import telebot
+
 import time
-import datetime
-import random
+import requests
+import telebot
 
-CHAVE_API = "5597794728:AAGfwOg3RijfPrQ5S_Iw6NKAuYucNEdIsO8" # BOT FOX
+ultimos_resultados = []
+check_resultados = []
+ultima_aposta = None  # Variável para rastrear a última aposta
 
-bot = telebot.TeleBot(CHAVE_API)
-
-group_id = '-1001970951473'
-
-
-
-links = [
-    "https://exemplo1.com",
-]
-
-
-possibilidades_minas = [
-    "1° e 2° dúzia",
-    "1° e 3° dúzia",
-    "2° e 3° dúzia",
-    "1° e 3° coluna",
-    "1° e 2° coluna",
-    "2° e 3° coluna"
-]
-
+token = '5597794728:AAGfwOg3RijfPrQ5S_Iw6NKAuYucNEdIsO8' #fox bot
+chat_id = '-1001970951473'
 
 
 texto4 = """
-ATENÇÃO VAMOS INICIAR !
+⚠️ <b>ANALISANDO ENTRADAS</b> ⚠️
+
+🎰 Roleta: Brasileira 
+
+<a href="https://affiliates.nuts.bet/visit/?bta=36979&brand=nutsbet"><b>🖥 Link de cadastro</b></a>
 """
 
 
-mensagem = """
-🔥 <b>Entrada: {}</b>
-🛡 Ate duas proteções - Cobrir o zero!
-🔗<a href="https://affiliates.nuts.bet/visit/?bta=36979&brand=nutsbet"> Cadastre-se antes de Jogar!</a>
-🎯 Entrada confirmada 🎯 
- Roleta: ROLETA BRASILEIRA 🎰
+
+# Função para puxar os dados da roleta
+def puxar_dados():
+    global ultimos_resultados
+    # Faz uma requisição GET para a URL específica
+    resposta = requests.get(url, headers=headers)
+    dic_resposta = resposta.json()
+    dados = dic_resposta['gameTables']
+    for i in dados:
+        roletas = i['gameTableId']
+        # Puxando os últimos resultados apenas da roleta brasileira
+        if '103910' in roletas:
+            chaves = i.keys()
+            if 'lastNumbers' in chaves:
+                ultimos_resultados = i['lastNumbers']
+    return ultimos_resultados
+
+# Função para determinar a cor do número (vermelho ou preto)
+def determinar_cor(numero):
+    vermelhos = ['1', '3', '5', '7', '9', '12', '14', '16', '18', '19', '21', '23', '25', '27', '30', '32', '34', '36']
+    if numero in vermelhos:
+        return "VERMELHO"
+    else:
+        return "PRETO"
+
+# Função para enviar mensagem para o Telegram
+def enviar_mensagem_telegram(mensagem):
+  
+    bot = telebot.TeleBot(token=token)
+    bot.send_message(chat_id=chat_id, text=mensagem, parse_mode='HTML', disable_web_page_preview=True)
+
+url = "https://casino.betfair.com/api/tables-details"
+headers = {"cookie": "vid=8ab7daa7-57f7-4196-8285-943390594163"}
+
+
+ultimos_resultados.clear()
+puxar_dados()
+
+if ultimos_resultados != check_resultados:
+    check_resultados = ultimos_resultados
+    primeiro_numero = ultimos_resultados[0]
+    segundo_numero = ultimos_resultados[1]
+    terceiro_numero = ultimos_resultados[2]
+    quarto_numero = ultimos_resultados[3]
+    x1 = determinar_cor(primeiro_numero)
+    x2 = determinar_cor(segundo_numero)
+    x3 = determinar_cor(terceiro_numero)
+    x4 = determinar_cor(quarto_numero)
+
+    if x1 == x2 == x3 == x4:
+        cor_aposta = "🔴" if x1 == "PRETO" else "⚫"
+        ultima_aposta = x1
+        mensagem = f"""
+        💰 <b>PADRÃO INDETIFICADO</b> 💰
+        🚀 Entrada Confirmada
+        🎰 Roleta: Brasileira 
+
+        Entrar na cor: {cor_aposta}
+
+        🟢 Sempre Cobrir o Zero
+        🚨 Aplicar até 2 gales
+    
+        <a href="https://affiliates.nuts.bet/visit/?bta=36979&brand=nutsbet"><b>🖥 Link de cadastro</b></a>
+
+        <a href="https://affiliates.nuts.bet/visit/?bta=36979&brand=nutsbet"><b>🎯 Aposte aqui!</b></a>
 """
+        enviar_mensagem_telegram(mensagem)
+        print(mensagem)
+        time.sleep(45)
+           
+            
+    else:
+        mensagem = f"O primeiro número foi: {primeiro_numero}, e ele é da cor: {x1}\n"
+        mensagem += f"O segundo número foi: {segundo_numero}, e ele é da cor: {x2}\n"
+        mensagem += f"O terceiro número foi: {terceiro_numero}, e ele é da cor: {x3}\n"
+        mensagem += f"O quarto número foi: {quarto_numero}, e ele é da cor: {x4}"
 
-print("======")
-possibilidade_mina_aleatoria = random.choice(possibilidades_minas)
-link_aleatorio = random.choice(links)
-mensagem_formatada = mensagem.format(possibilidade_mina_aleatoria)
-mensagem_formatada = mensagem_formatada.replace("LINK_PLATAFORMA_CORRETA", link_aleatorio)
-mensagem_formatada = mensagem_formatada.replace("LINK_JOGO", link_aleatorio)
+       
+    print(mensagem)
 
-bot.send_message(chat_id=group_id, text=mensagem_formatada, parse_mode='HTML', disable_web_page_preview=True)
-time.sleep(600)
+if ultima_aposta is not None:
+        puxar_dados()
+        novo_resultado = determinar_cor(ultimos_resultados[0])
+        if novo_resultado == ultima_aposta:
+            mensagem_aposta_vencedora = f" RED ❌"
+            enviar_mensagem_telegram(mensagem_aposta_vencedora)
+            ultima_aposta = None  # Limpa a última aposta
+            time.sleep(10)
+             
+            bot = telebot.TeleBot(token=token)
+            bot.send_message(chat_id=chat_id, text=texto4, parse_mode='HTML', disable_web_page_preview=True)
+
+           
+        else:
+            mensagem_aposta_perdedora = f" GREENN ✅✅"
+            enviar_mensagem_telegram(mensagem_aposta_perdedora)
+            ultima_aposta = None  # Limpa a última aposta
+            time.sleep(10)
+              
+            bot = telebot.TeleBot(token=token)
+            bot.send_message(chat_id=chat_id, text=texto4, parse_mode='HTML', disable_web_page_preview=True)
+
+   
+time.sleep(5)
