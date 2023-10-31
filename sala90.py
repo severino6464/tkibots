cd873fc55826c52642215bd348d02f8728935c1a
severino tiburcio (TKI TECNOLOGIA) <46749707+severino6464@users.noreply.github.com>
2023-10-31 14:20:29 -0300

    Atualização do arquivo

--- sala90.py
+++ sala90.py
@@ -16,7 +16,7 @@ texto4 = """
 🐯 Fortune Tiger - Entrada em 2 minutos
 🔎 Estamos validando uma entrada
 
-[📱 Cadastre-se aqui](https://affiliates.nuts.bet/visit/?bta=35816&brand=nutsbet)
+[📱 Cadastre-se aqui](https://affiliates.nuts.bet/visit/?bta=36369&brand=nutsbet)
 """
 
 texto5 = """
@@ -36,30 +36,30 @@ mensagem = """
 
 🌪 Faça no máximo {} jogadas!
 
-[📱 Cadastre-se aqui](https://affiliates.nuts.bet/visit/?bta=35816&brand=nutsbet)
+[📱 Cadastre-se aqui]https://affiliates.nuts.bet/visit/?bta=36369&brand=nutsbet)
 
-[📱 Jogar Fortune Tiger🐯☘](https://affiliates.nuts.bet/visit/?bta=35816&brand=nutsbet)
+[📱 Jogar Fortune Tiger🐯☘](https://affiliates.nuts.bet/visit/?bta=36369&brand=nutsbet)
 """
 
 
 
-while True:
-
-    print("BOT-aff91-nuts")
-
-    bot.send_message(chat_id=group_id, text=texto4, parse_mode='Markdown')
-    time.sleep(120) 
 
 
+print("BOT-aff91-nuts")
 
-    n_jogadas = random.randint(2, 20)
-    validade = datetime.datetime.now() + datetime.timedelta(minutes=10)
-    hora_validade = validade.strftime("%H:%M")
-    mensagem_formatada = mensagem.format(n_jogadas, hora_validade, n_jogadas)
+bot.send_message(chat_id=group_id, text=texto4, parse_mode='Markdown')
+time.sleep(120) 
 
-    bot.send_message(chat_id=group_id, text=mensagem_formatada, parse_mode='Markdown')
 
-    time.sleep(60)  # Espera 5 minutos (300 segundos)
 
-    bot.send_message(chat_id=group_id, text=texto5, parse_mode='Markdown')
-    time.sleep(120) 
+n_jogadas = random.randint(2, 20)
+validade = datetime.datetime.now() + datetime.timedelta(minutes=10)
+hora_validade = validade.strftime("%H:%M")
+mensagem_formatada = mensagem.format(n_jogadas, hora_validade, n_jogadas)
+
+bot.send_message(chat_id=group_id, text=mensagem_formatada, parse_mode='Markdown')
+
+time.sleep(60)  # Espera 5 minutos (300 segundos)
+
+bot.send_message(chat_id=group_id, text=texto5, parse_mode='Markdown')
+time.sleep(120) 
