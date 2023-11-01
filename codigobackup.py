c325714ae693855bfe63bca30403956d5a9d80c0
severino tiburcio (TKI TECNOLOGIA) <46749707+severino6464@users.noreply.github.com>
2023-11-01 13:43:36 -0300

    01.11

--- codigobackup.py
+++ codigobackup.py
@@ -1,20 +1,33 @@
 import subprocess
-import time
 import os
+import threading
+
+
+# Função para executar um código de sala em loop
+def executar_sala(sala_codigo):
+    while True:
+        subprocess.run(["python3", os.path.join(base_path, sala_codigo)])
 
 # Número total de salas
-num_salas = 3
+num_salas = 700
 
 # Caminho para a pasta onde o código está sendo executado
 base_path = os.path.dirname(os.path.abspath(__file__))
 
-# Loop para executar os códigos de cada sala
+# Lista para armazenar as threads
+threads = []
+
+# Loop para criar threads para cada sala e iniciar a execução em loop
 for sala_numero in range(1, num_salas + 1):
     sala_codigo = f"sala{sala_numero}.py"
-    subprocess.run(["python3", os.path.join(base_path, sala_codigo)])
 
-# Intervalo de tempo em segundos após executar todas as salas
-intervalo_no_final = 1  # por exemplo, 5 minutos
 
-# Aguardar o intervalo de tempo após executar todas as salas
-time.sleep(intervalo_no_final)
+    
+    thread = threading.Thread(target=executar_sala, args=(sala_codigo,))
+    threads.append(thread)
+    thread.start()
+    
+
+# Aguardar todas as threads terminarem (que não acontecerá nesse caso)
+for thread in threads:
+    thread.join()
