ca7ccb332e2cb2a0685b1bb32a0d5a9ebfdd47b9
severino tiburcio (TKI TECNOLOGIA) <46749707+severino6464@users.noreply.github.com>
2023-11-09 18:02:42 -0300

    Atualização do arquivo

---
+++ sala5.py
@@ -0,0 +1,109 @@
+import requests
+import time
+import random
+import datetime
+
+mensagem_1 = """
+⚠️ Fique atento ao jogo ⚠️
+ 💣 Mines
+🔎 Estamos validando uma entrada
+"""
+
+mensagem_2 = """
+🎲 Entrada confirmada 🎲
+🥇: Entrada 
+
+{}
+🎮: Tentativas: 2
+Jogar com 2 a 3 minas
+
+📲: Plataforma correta: [Clique aqui]({})
+👉🏻: Link do jogo: [Mines]({})
+⏱️ Válido até: {}
+
+"""
+
+links = [
+    "https://nuts.bet/casino",
+    
+]
+
+
+text2 = """
+🔷🔹 Entrada Finalizada 🔹🔷
+     ✅✅ GRENN! ✅✅
+  """
+
+possibilidades_minas = [
+    "💣⭐️⭐️💣💣\n💣💣⭐️💣💣\n💣💣💣💣💣\n💣💣⭐️⭐️⭐️\n💣💣⭐️💣💣",
+    "💣💣⭐️💣⭐️\n💣⭐️⭐️💣💣\n💣⭐️⭐️💣💣\n💣💣⭐️💣💣\n💣💣⭐️💣💣",
+    "⭐️💣⭐️💣💣\n💣💣⭐️💣💣\n💣💣💣💣💣\n💣💣⭐️⭐️⭐️\n💣💣⭐️💣💣",
+    "💣💣💣⭐️⭐️\n💣💣💣⭐️💣\n💣⭐️⭐️⭐️💣\n💣💣⭐️💣💣\n💣💣⭐️💣💣",
+    "⭐️💣💣💣⭐️\n💣💣⭐️💣💣\n💣💣💣💣💣\n💣⭐️⭐️💣💣\n💣💣⭐️💣💣",
+    "⭐️💣💣💣⭐️\n💣💣⭐️💣💣\n💣💣💣💣💣\n💣💣⭐️💣💣\n💣💣⭐️💣💣",
+    "💣⭐️💣💣💣\n⭐️💣⭐️💣💣\n💣⭐️⭐️💣💣\n💣💣💣💣💣\n💣💣⭐️💣💣",
+    "💣⭐️💣⭐️💣\n💣💣💣⭐️💣\n💣💣⭐️⭐️💣\n💣💣⭐️⭐️💣\n💣💣⭐️💣💣",
+    "⭐️⭐️⭐️💣💣\n💣⭐️⭐️💣💣\n💣💣💣💣💣\n💣💣⭐️💣💣\n💣💣⭐️💣💣",
+    "💣💣💣💣💣\n💣💣💣💣⭐️\n⭐️💣⭐️💣💣\n💣💣⭐️💣💣\n💣💣⭐️💣💣",
+    "⭐️⭐️💣💣💣\n💣⭐️💣💣💣\n💣⭐️⭐️⭐️💣\n💣💣💣💣💣\n💣💣💣💣💣",
+    "⭐️⭐️💣⭐️💣\n💣💣💣⭐️⭐️\n💣💣💣💣💣\n💣💣💣💣💣\n💣⭐️⭐️⭐️💣",
+    "💣💣💣💣⭐️\n⭐️💣💣⭐️💣\n⭐️⭐️⭐️💣💣\n💣⭐️💣💣💣\n💣⭐️💣💣💣",
+    "⭐️💣💣⭐️💣\n💣💣💣💣💣\n💣⭐️💣⭐️💣\n💣⭐️💣💣💣\n💣⭐️💣⭐️💣",
+    "⭐️⭐️⭐️💣💣\n⭐️💣⭐️💣💣\n💣💣💣💣💣\n💣💣💣💣💣\n💣💣💣⭐️💣",
+    "⭐️💣💣💣💣\n⭐️💣💣⭐️💣\n💣💣💣💣💣\n💣💣⭐️⭐️💣\n💣💣⭐️⭐️⭐️",
+    "💣💣💣💣⭐️\n⭐️💣💣⭐️💣\n⭐️💣⭐️⭐️💣\n💣💣💣💣💣\n💣💣⭐️⭐️💣",
+    "💣💣💣⭐️💣\n⭐️💣💣⭐️⭐️\n⭐️💣💣💣💣\n💣💣💣💣💣\n💣💣💣⭐️💣",
+    "⭐️💣💣💣💣\n💣💣💣💣💣\n⭐️💣💣💣💣\n💣⭐️💣⭐️💣\n💣⭐️💣⭐️💣"
+]
+
+
+
+
+  
+def enviar_mensagens():
+    url = 'https://api.z-api.io/instances/3C6139F323A8F08189B42A5707B8D550/token/A705A869C06CC34BF55690C8/send-text'
+    headers = {
+        'Client-Token': 'F9dfef0feecb44fbe8ad1accdb380e4f7S',
+        'Content-Type': 'application/json'
+    }
+
+    # Enviar a mensagem_1
+    data = {
+        "phone": "120363179086979521-group",
+        "message": mensagem_1
+    }
+    response = requests.post(url, headers=headers, json=data)
+
+    if response.status_code == 200:
+        print("Mensagem 1 enviada com sucesso!")
+    else:
+        print(f"Falha ao enviar a mensagem 1. Código de status: {response.status_code}")
+
+    time.sleep(120)  # Aguardar 2 minutos
+
+    # Enviar a mensagem_2 formatada
+    possibilidade_mina_aleatoria = random.choice(possibilidades_minas)
+    link_aleatorio = random.choice(links)
+    validade = datetime.datetime.now() + datetime.timedelta(minutes=5)
+    hora_validade = validade.strftime("%H:%M")
+    mensagem_formatada = mensagem_2.format(possibilidade_mina_aleatoria, link_aleatorio, link_aleatorio, hora_validade)
+
+    data = {
+        "phone": "120363179086979521-group",
+        "message": mensagem_formatada
+    }
+
+    response = requests.post(url, headers=headers, json=data)
+
+    if response.status_code == 200:
+        print("Mensagem 2 formatada enviada com sucesso!")
+    else:
+        print(f"Falha ao enviar a mensagem 2 formatada. Código de status: {response.status_code}")
+
+    time.sleep(300)  # Aguardar 40 segundos após o envio da mensagem formatada
+
+
+enviar_mensagens()
+print("Todas as mensagens foram enviadas. Reiniciando o ciclo.\n")
+    
+    
