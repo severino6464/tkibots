8b4e4d4644f38ad5c7537d850c8dd10294d1d546
severino tiburcio (TKI TECNOLOGIA) <46749707+severino6464@users.noreply.github.com>
2023-11-16 12:43:47 -0300

    Update sala675.py

--- sala675.py
+++ sala675.py
@@ -3,7 +3,7 @@ import time
 import datetime
 import random
 
-CHAVE_API = "5597794728:AAGfwOg3RijfPrQ5S_Iw6NKAuYucNEdIsO8" # BOT FOX
+CHAVE_API = "5597794728:AAGfwOg3RijfPrQ5S_Iw6NKAuYucNEdIsO8"  # BOT FOX
 
 bot = telebot.TeleBot(CHAVE_API)
 
@@ -16,19 +16,28 @@ def gerar_possibilidades_minas():
     for _ in range(3):
         # Inicializa a matriz com "🟢" para representar células vazias
         matriz_mina = [["🟢"] * 5 for _ in range(3)]
-        
+
         # Escolhe aleatoriamente uma linha e uma coluna para a bola (⚽️)
         linha = random.randint(0, 2)
         coluna = random.randint(0, 4)
-        
+
         # Define a posição da bola na matriz
         matriz_mina[linha][coluna] = "⚽️"
-        
+
         # Formata a matriz como texto
         possibilidade_mina = "\n".join("".join(linha) for linha in matriz_mina)
         possibilidades_minas.append(possibilidade_mina)
 
     return possibilidades_minas
+
+selecoes = [
+    "Argentina", "Áustria", "Bélgica", "Brasil",
+    "Croácia", "Tcheco", "Dinamarca", "Inglaterra",
+    "Finlândia", "França", "Alemanha", "Irlanda",
+    "Itália", "Holanda", "Polônia", "Portugal",
+    "Escócia", "Sérvia", "Espanha", "Suécia",
+    "Suíça", "Turquia", "Ucrânia", "Uruguai"
+]
 
 texto4 = """
 ⚠️ <b>Fique atento ao jogo</b> ⚠️
@@ -46,7 +55,7 @@ texto5 = """
 
 mensagem = """
 ⚽️💰 <b>Entrada confirmada</b> ⚽️💰
-🏁 Seleção: Portugal
+🏁 Seleção: {}
 ⏰ Válido até: {}
 🔁 N° de tentativas: {}
 🔗 Link de acesso: <a href="https://affiliates.nuts.bet/visit/?bta=37127&nci=5386"><b>Penalty Shoot-Out-Street</b></a>
@@ -66,7 +75,8 @@ mensagem_formatada = "\n\n".join(possibilidades_minas)
 n_jogadas = random.randint(1, 3)
 validade = datetime.datetime.now() + datetime.timedelta(minutes=2)
 hora_validade = validade.strftime("%H:%M")
-mensagem_formatada = mensagem.format(hora_validade, n_jogadas, mensagem_formatada)
+selecao_aleatoria = random.choice(selecoes)
+mensagem_formatada = mensagem.format(selecao_aleatoria, hora_validade, n_jogadas, mensagem_formatada)
 bot.send_message(chat_id=group_id, text=mensagem_formatada, parse_mode='HTML', disable_web_page_preview=True)
 time.sleep(120)  # Espera 5 minutos (300 segundos)
 
