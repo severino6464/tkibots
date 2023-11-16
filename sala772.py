82f995aeabefe05f9c4db56418517cb4223a7dce
severino tiburcio (TKI TECNOLOGIA) <46749707+severino6464@users.noreply.github.com>
2023-11-16 17:35:21 -0300

    Update sala772.py

--- sala772.py
+++ sala772.py
@@ -1,13 +1,13 @@
+import random
+import requests
 import telebot
 import datetime
-import random
 import time
-import requests
 
 CHAVE_API = "5597794728:AAGfwOg3RijfPrQ5S_Iw6NKAuYucNEdIsO8"  # Substitua pelo seu token de bot
 bot = telebot.TeleBot(CHAVE_API)
 
-channel_id = '-1002052729835'   # Substitua pelo ID do seu canal
+channel_id = '-1002052729835'  # Substitua pelo ID do seu canal
 
 possibilidades_minas = [
     "Apostar na 1° e 2° dúzia",
@@ -16,9 +16,9 @@ possibilidades_minas = [
 ]
 
 url = "https://casino.betfair.com/api/tables-details"
-headers = {"cookie": "vid=8ab7daa7-57f7-4196-8285-943390594163"}
+headers = {"cookie": "vid=YOUR_COOKIE"}
 
-def puxar_dados():
+def puxar_ultimo_numero():
     resposta = requests.get(url, headers=headers)
     dic_resposta = resposta.json()
     dados = dic_resposta['gameTables']
@@ -28,7 +28,9 @@ def puxar_dados():
             chaves = i.keys()
             if 'lastNumbers' in chaves:
                 ultimos_resultados = i['lastNumbers']
-                return ultimos_resultados
+                if ultimos_resultados:
+                    return ultimos_resultados[0]
+    return None
 
 texto4 = """
 ⚠️ <b>ATENÇÃO VAMOS INICIAR</b> ⚠️
@@ -45,74 +47,65 @@ mensagem = """
 🔥 Entrar: {}
 
 🧨 Último número: {}
-⏱️ Válido até: {}
 
-<a href="https://nuts.bet/live-casino/game/2177465"><b>💸 Clique aqui para abrir a corretora!</b></a>
-"""
-
-texto5 = """
-<b>✅🤑BATEUUUU🤑✅</b>
+<a href="https://affiliates.nuts.bet/visit/?bta=38535&brand=nutsbet"><b>💸 Clique aqui para abrir a corretora!</b></a>
 """
 
 # Lista para armazenar os sinais enviados
 sinais_enviados = []
 
-def enviar_sinal(resultado, contagem_verde, contagem_vermelho):
-    ultimos_resultados = puxar_dados()
+def enviar_sinal():
+    ultimos_resultados = puxar_ultimo_numero()
     if ultimos_resultados:
-        ultimo_numero = ultimos_resultados[0]
+        ultimo_numero = ultimos_resultados
         possibilidade_mina_aleatoria = random.choice(possibilidades_minas)
-        mensagem_formatada = mensagem.format(possibilidade_mina_aleatoria, ultimo_numero, resultado['hora'])
+        hora = datetime.datetime.now().strftime("%H:%M")
+        resultado = 'GREEN ✅' if random.random() < 0.8 else 'RED ❌'  # Exemplo aleatório
+
+        # Esperar 5 segundos antes de enviar a mensagem
+        time.sleep(5)
+
+        mensagem_formatada = mensagem.format(possibilidade_mina_aleatoria, ultimo_numero, hora)
         bot.send_message(chat_id=channel_id, text=mensagem_formatada, parse_mode='HTML', disable_web_page_preview=True)
-        sinais_enviados.append(resultado)
+        sinais_enviados.append({'hora': hora, 'resultado': resultado})
+
+def gerar_relatorio():
+    data_atual = datetime.datetime.now().strftime("%d/%m/%Y")
+    relatorio = f"📔 RELATÓRIO DE OPERAÇÕES ({data_atual})\n\n"
+
+    contagem_verde = 0
+    contagem_vermelho = 0
+
+    for resultado in sinais_enviados:
+        relatorio += f"{resultado['hora']} - {resultado['resultado']}\n"
+
         if resultado['resultado'] == 'GREEN ✅':
             contagem_verde += 1
         elif resultado['resultado'] == 'RED ❌':
             contagem_vermelho += 1
-        return contagem_verde, contagem_vermelho
-
-def gerar_relatorio(contagem_verde, contagem_vermelho):
-    data_atual = datetime.datetime.now().strftime("%d/%m/%Y")
-    relatorio = f"📔 RELATÓRIO DE OPERAÇÕES ({data_atual})\n\n"
-
-    for resultado in sinais_enviados:
-        relatorio += f"{resultado['hora']} - {resultado['resultado']}\n"
 
     placar = f"\nPlacar: ✅ {contagem_verde} x ❌ {contagem_vermelho}\n"
     relatorio += placar
 
     return relatorio
 
-def enviar_relatorio(contagem_verde, contagem_vermelho):
-    relatorio = gerar_relatorio(contagem_verde, contagem_vermelho)
+
+def enviar_relatorio():
+    relatorio = gerar_relatorio()
     bot.send_message(chat_id=channel_id, text=relatorio, parse_mode='HTML', disable_web_page_preview=True)
 
 def enviar_sinais_e_relatorio():
-    contagem_verde = 0
-    contagem_vermelho = 0
-
     # Enviar o texto4 antes dos sinais
     bot.send_message(chat_id=channel_id, text=texto4, parse_mode='HTML', disable_web_page_preview=True)
 
     # Aguardar 5 minutos antes de iniciar os sinais
     time.sleep(5 * 60)
 
-    for _ in range(6):
-        resultado = {'hora': datetime.datetime.now().strftime("%H:%M"), 'resultado': 'GREEN ✅' if contagem_verde < 5 else 'RED ❌'}
-        
-        # Enviar uma entrada confirmada
-        enviar_sinal(resultado, contagem_verde, contagem_vermelho)
-        
-        if resultado['resultado'] == 'GREEN ✅':
-            # Aguardar 60 segundos antes de enviar texto5
-            time.sleep(120)
-            bot.send_message(chat_id=channel_id, text=texto5, parse_mode='HTML', disable_web_page_preview=True)
-        
+    for _ in range(5):
+        enviar_sinal()
         time.sleep(10 * 60)
 
-    enviar_relatorio(contagem_verde, contagem_vermelho)
-
-
+    enviar_relatorio()
 
 def check_and_send_signal():
     current_time = datetime.datetime.now().strftime("%H:%M")
@@ -123,4 +116,3 @@ def check_and_send_signal():
 
 # Chame a função check_and_send_signal a partir do seu código principal sempre que necessário
 check_and_send_signal()
-
