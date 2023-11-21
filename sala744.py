df9019a9854feb565b9b28e3ec11f67fb3c4ea99
severino tiburcio (TKI TECNOLOGIA) <46749707+severino6464@users.noreply.github.com>
2023-11-21 12:51:24 -0300

    Update sala744.py

--- sala744.py
+++ sala744.py
@@ -20,16 +20,20 @@ ATENÇÃO!
 
 texto2 = """
 ENTRADA CONFIRMADA!
-Validade: 3 minutos
+{}x normal e {}x turbo
 """
 
 texto3 = """
 ENTRADA ENCERRADA!
 """
 
-
 bot.send_message(chat_id=group_id, text=texto1, parse_mode='HTML', disable_web_page_preview=True)
 time.sleep(10)
-bot.send_message(chat_id=group_id, text=texto2, parse_mode='HTML', disable_web_page_preview=True)
+
+n_jogadas = random.randint(2, 6)
+n_jogadas2 = random.randint(3, 8)
+mensagem_formatada = texto2.format(n_jogadas, n_jogadas2)
+bot.send_message(chat_id=group_id, text=mensagem_formatada, parse_mode='HTML', disable_web_page_preview=True)
+
 time.sleep(120)
 bot.send_message(chat_id=group_id, text=texto3, parse_mode='HTML', disable_web_page_preview=True)
