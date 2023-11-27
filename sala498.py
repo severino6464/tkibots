40cd7059ca63e34db7b963266f4634cd5f8e8cec
severino tiburcio (TKI TECNOLOGIA) <46749707+severino6464@users.noreply.github.com>
2023-11-27 13:47:58 -0300

    Update sala498.py

--- sala498.py
+++ sala498.py
@@ -67,7 +67,10 @@ text2 = """
   """
 
 text3 = """
-<b>Deposite 30 reais e clique no link abaixo para entrar nas lives e concorrer a prêmios 👇
+<b>✅5 GREENS AO VIVO TODOS OS DIAS DE GRAÇA
+
+Deposite 30 reais e clique no link abaixo para entrar nas lives e concorrer a prêmios 👇
+
 </b> 
 <a href="https://t.me/+P4rH7M6Iz0QyOWIx">CLIQUE AQUI</a>
   """
