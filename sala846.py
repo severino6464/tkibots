b06541a5ac2a4e53b68a816b6e28d73e1945c86c
severino tiburcio (TKI TECNOLOGIA) <46749707+severino6464@users.noreply.github.com>
2023-11-28 23:25:27 -0300

    Update sala846.py

--- sala846.py
+++ sala846.py
@@ -6,7 +6,7 @@ CHAVE_API = "5597794728:AAGfwOg3RijfPrQ5S_Iw6NKAuYucNEdIsO8"  # BOT FOX
 
 bot = telebot.TeleBot(CHAVE_API)
 
-group_id = '-1001949184128'
+group_id = '-100205652468'
 
 texto4 = """
 ⚠️ <b>OPORTUNIDADES IDENTIFICADAS!</b> ⚠️
