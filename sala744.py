54253af32a62c1824eb8375ce6f52339198c511a
severino tiburcio (TKI TECNOLOGIA) <46749707+severino6464@users.noreply.github.com>
2023-12-02 01:43:20 -0300

    Update sala744.py

--- sala744.py
+++ sala744.py
@@ -3,7 +3,7 @@ import time
 import datetime
 import random
 
-CHAVE_API = "5597794728:AAGfwOg3RijfPrQ5S_Iw6NKAuYucNEdIsO8" # BOT FOX
+CHAVE_API = "5597794728:AAGfwOg3RijfPrQ5S_Iw6NKAuYucNEdIsO8"  # BOT FOX
 
 bot = telebot.TeleBot(CHAVE_API)
 
@@ -11,23 +11,24 @@ group_id = '-1002023633024'
 
 sticker_file_id = 'CAACAgEAAxkBAAMCZSbmh4EopfmSJgx8Z8sDxkeWf1UAAvwAAzgOghFAju2fQymOBzAE'
 
-
-
 texto1 = """
 ATENÇÃO!
-
 """
 
-texto2 = """
+# Gerar valores aleatórios para a entrada normal e turbo
+entrada_normal = random.randint(3, 11)
+entrada_turbo = random.randint(5, 15)
+
+texto2 = f"""
 ENTRADA CONFIRMADA!
-Entrar:
+Entrar: {entrada_normal}x normal e {entrada_turbo}x turbo
 Validade: 3 minutos
+
 """
 
 texto3 = """
 ENTRADA ENCERRADA
 """
-
 
 bot.send_message(chat_id=group_id, text=texto1, parse_mode='HTML', disable_web_page_preview=True)
 time.sleep(10)
