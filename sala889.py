32fd4f969049250f02b595b444858b1ef498e121
severino tiburcio (TKI TECNOLOGIA) <46749707+severino6464@users.noreply.github.com>
2023-12-03 09:04:54 -0300

    Update sala889.py

--- sala889.py
+++ sala889.py
@@ -3,18 +3,11 @@ import time
 import datetime
 import random
 
-CHAVE_API = "5597794728:AAGfwOg3RijfPrQ5S_Iw6NKAuYucNEdIsO8" # BOT FOX
+CHAVE_API = "5597794728:AAGfwOg3RijfPrQ5S_Iw6NKAuYucNEdIsO8"  # BOT FOX
 
 bot = telebot.TeleBot(CHAVE_API)
 
 group_id = '-1001622316632'
-
-sticker_file_id = 'CAACAgIAAxkBAAMmZSb_ngXS-jrJPaIDkQxNkCtYOQQAAtgLAAJYD5hKNPj69b5xWK8wBA'
-
-links = [
-    "https://exemplo1.com",
-]
-
 
 possibilidades_minas = [
     "Apostar em números baixos [1-18]",
@@ -25,13 +18,20 @@ possibilidades_minas = [
 ]
 
 
-
 texto4 = """
-⚠️ <b>ATENÇÃO VAMOS INICIAR!</b> ⚠️
+⚠️ <b>OPORTUNIDADES IDENTIFICADAS!</b> ⚠️
 """
 
+texto5 = """
+     <b>Entradas Finalizadas!!!!</b>
+✅✅ <b>LUCROOO!</b> ✅✅
+"""
 
-mensagem = """
+# Estrutura de dados para os animais
+animais = [
+    {
+        'nome': 'Roleta Brasileira',
+        'mensagem': """
 🔥 <b>ROBÔ CONFIRMOU</b> 🔥
 🎰 Roleta: Brasileira
 
@@ -44,23 +44,63 @@ mensagem = """
 
 <a href="https://affiliates.nuts.bet/visit/?bta=38796&brand=nutsbet">🔗 Cadastre-se antes de Jogar!</a>
 
+<a href="https://nuts.bet/live-casino/game/2177465">🖥 Jogue aqui!</a>
 """
+    },
+    {
+        'nome': 'American Roulette',
+        'mensagem': """
+🔥 <b>ROBÔ CONFIRMOU</b> 🔥
+🎰 Roleta: American Roulette
 
+{}
 
+⏱️ Válido até: {}
 
+👉 Cobrir o zero
+🔁 Fazer até 3 gales
 
-print("======")
+<a href="https://affiliates.nuts.bet/visit/?bta=38796&brand=nutsbet">🔗 Cadastre-se antes de Jogar!</a>
+
+<a href="https://nuts.bet/live-casino/game/2177465">🖥 Jogue aqui!</a>
+"""
+    },
+    {
+        'nome': 'Mega Fire Blaze Roullete Live',
+        'mensagem': """
+🔥 <b>ROBÔ CONFIRMOU</b> 🔥
+🎰 Roleta: Mega Fire Blaze Roullete Live
+
+{}
+
+⏱️ Válido até: {}
+
+👉 Cobrir o zero
+🔁 Fazer até 3 gales
+
+<a href="https://affiliates.nuts.bet/visit/?bta=38796&brand=nutsbet">🔗 Cadastre-se antes de Jogar!</a>
+
+<a href="https://nuts.bet/live-casino/game/2177465">🖥 Jogue aqui!</a>
+"""
+    }
+]
+
+print("=======")
 bot.send_message(chat_id=group_id, text=texto4, parse_mode='HTML', disable_web_page_preview=True)
 time.sleep(60)
-possibilidade_mina_aleatoria = random.choice(possibilidades_minas)
-link_aleatorio = random.choice(links)
-validade = datetime.datetime.now() + datetime.timedelta(minutes=5)
-hora_validade = validade.strftime("%H:%M")
-mensagem_formatada = mensagem.format(possibilidade_mina_aleatoria, hora_validade)
-mensagem_formatada = mensagem_formatada.replace("LINK_PLATAFORMA_CORRETA", link_aleatorio)
-mensagem_formatada = mensagem_formatada.replace("LINK_JOGO", link_aleatorio)
 
-bot.send_message(chat_id=group_id, text=mensagem_formatada, parse_mode='HTML', disable_web_page_preview=True)
-time.sleep(120)
-bot.send_sticker(chat_id=group_id, sticker=sticker_file_id)
-time.sleep(420)
+# Itera sobre os animais e envia as mensagens
+for animal in animais:
+    possibilidade_mina_aleatoria = random.choice(possibilidades_minas)
+    validade = datetime.datetime.now() + datetime.timedelta(minutes=5)
+    hora_validade = validade.strftime("%H:%M")
+
+    mensagem_formatada = animal['mensagem'].format(
+        possibilidade_mina_aleatoria, hora_validade)
+
+    bot.send_message(chat_id=group_id, text=mensagem_formatada,
+                     parse_mode='HTML', disable_web_page_preview=True)
+    time.sleep(360)
+
+bot.send_message(chat_id=group_id, text=texto5, parse_mode='HTML', disable_web_page_preview=True)
+time.sleep(480)
