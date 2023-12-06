74385ab074a0900d82b64fc849c86ceccaef5919
severino tiburcio (TKI TECNOLOGIA) <46749707+severino6464@users.noreply.github.com>
2023-12-06 15:41:14 -0300

    Update sala871.py

--- sala871.py
+++ sala871.py
@@ -3,72 +3,75 @@ import time
 import datetime
 import random
 
-CHAVE_API = "5597794728:AAGfwOg3RijfPrQ5S_Iw6NKAuYucNEdIsO8" # BOT FOX
+CHAVE_API = "5597794728:AAGfwOg3RijfPrQ5S_Iw6NKAuYucNEdIsO8" 
 
 bot = telebot.TeleBot(CHAVE_API)
 
 group_id = '-1002005446042'
 
-sticker_file_id = 'CAACAgIAAxkBAAMfZSb4VppXyng9TmBwSA12Ss3y2hkAAhoDAAKc1ucKpX8qfdF9KAIwBA'
+sticker_file_id = 'CAACAgIAAxkBAAMmZSb_ngXS-jrJPaIDkQxNkCtYOQQAAtgLAAJYD5hKNPj69b5xWK8wBA'
 
-texto4 = """
-🎲 Fique atento ao jogo 🎲
+possibilidades_minas = [
+    "💣⭐️⭐️💣💣\n💣💣⭐️💣💣\n💣💣💣💣💣\n💣💣⭐️⭐️⭐️\n💣💣⭐️💣💣",
+    "💣💣⭐️💣⭐️\n💣⭐️⭐️💣💣\n💣⭐️⭐️💣💣\n💣💣⭐️💣💣\n💣💣⭐️💣💣",
+    "⭐️💣⭐️💣💣\n💣💣⭐️💣💣\n💣💣💣💣💣\n💣💣⭐️⭐️⭐️\n💣💣⭐️💣💣",
+    "💣💣💣⭐️⭐️\n💣💣💣⭐️💣\n💣⭐️⭐️⭐️💣\n💣💣⭐️💣💣\n💣💣⭐️💣💣",
+    "⭐️💣💣💣⭐️\n💣💣⭐️💣💣\n💣💣💣💣💣\n💣⭐️⭐️💣💣\n💣💣⭐️💣💣",
+    "⭐️💣💣💣⭐️\n💣💣⭐️💣💣\n💣💣💣💣💣\n💣💣⭐️💣💣\n💣💣⭐️💣💣",
+    "💣⭐️💣💣💣\n⭐️💣⭐️💣💣\n💣⭐️⭐️💣💣\n💣💣💣💣💣\n💣💣⭐️💣💣",
+    "💣⭐️💣⭐️💣\n💣💣💣⭐️💣\n💣💣⭐️⭐️💣\n💣💣⭐️⭐️💣\n💣💣⭐️💣💣",
+    "⭐️⭐️⭐️💣💣\n💣⭐️⭐️💣💣\n💣💣💣💣💣\n💣💣⭐️💣💣\n💣💣⭐️💣💣",
+    "💣💣💣💣💣\n💣💣💣💣⭐️\n⭐️💣⭐️💣💣\n💣💣⭐️💣💣\n💣💣⭐️💣💣",
+    "⭐️⭐️💣💣💣\n💣⭐️💣💣💣\n💣⭐️⭐️⭐️💣\n💣💣💣💣💣\n💣💣💣💣💣",
+    "⭐️⭐️💣⭐️💣\n💣💣💣⭐️⭐️\n💣💣💣💣💣\n💣💣💣💣💣\n💣⭐️⭐️⭐️💣",
+    "💣💣💣💣⭐️\n⭐️💣💣⭐️💣\n⭐️⭐️⭐️💣💣\n💣⭐️💣💣💣\n💣⭐️💣💣💣",
+    "⭐️💣💣⭐️💣\n💣💣💣💣💣\n💣⭐️💣⭐️💣\n💣⭐️💣💣💣\n💣⭐️💣⭐️💣",
+    "⭐️⭐️⭐️💣💣\n⭐️💣⭐️💣💣\n💣💣💣💣💣\n💣💣💣💣💣\n💣💣💣⭐️💣",
+    "⭐️💣💣💣💣\n⭐️💣💣⭐️💣\n💣💣💣💣💣\n💣💣⭐️⭐️💣\n💣💣⭐️⭐️⭐️",
+    "💣💣💣💣⭐️\n⭐️💣💣⭐️💣\n⭐️💣⭐️⭐️💣\n💣💣💣💣💣\n💣💣⭐️⭐️💣",
+    "💣💣💣⭐️💣\n⭐️💣💣⭐️⭐️\n⭐️💣💣💣💣\n💣💣💣💣💣\n💣💣💣⭐️💣",
+    "⭐️💣💣💣💣\n💣💣💣💣💣\n⭐️💣💣💣💣\n💣⭐️💣⭐️💣\n💣⭐️💣⭐️💣"
+]
 
-🐂 Fortune OX
-🔎 Estamos validando uma entrada
+links = [
+    "https://exemplo1.com",
+   
+]
 
-<a href="https://affiliates.nuts.bet/visit/?bta=38742&brand=nutsbet">📱 Cadastre-se aqui</a>
-
-🚨O sinal do robô só funciona apenas na plataforma acima! Faça💰🤑
-
-
-"""
-
-texto5 = """
-🔷🔹 Entrada Finalizada 🔹🔷
-     ✅✅ GRENN! ✅✅
- 
-"""
 
 
 mensagem = """
-⚠️ ENTRADA CONFIRMADA ⚠️
+🎲 Entrada confirmada 🎲
+🥇: Entrada 
 
-🐂 Fortune OX
+{}
+🎮: Tentativas: 2
+Jogar com 2 a 3 minas
 
-🔥 𝗡º 𝗱𝗲 𝗝𝗼𝗴𝗮𝗱𝗮𝘀: {}
-🔹 {}X modo Normal
-🔹 {}X modo Turbo
-⏰ Sinal válido até: {}
+⏱️ Válido até: {}
 
-
-🌪 Faça no máximo {} jogadas!
-
-<a href="https://affiliates.nuts.bet/visit/?bta=38742&brand=nutsbet">📱 Cadastre-se aqui</a>
-
-🚨O sinal do Robô só funciona apenas na plataforma acima! Faça💰🤑
+<a href="https://affiliates.nuts.bet/visit/?bta=38742&brand=nutsbet">📲: Plataforma correta</a>
+<a href="https://nuts.bet/casino/game/2172765">👉🏻: Link do jogo</a>
 """
 
-print("======")
 
-bot.send_message(chat_id=group_id, text=texto4, parse_mode='HTML', disable_web_page_preview=True)
-time.sleep(60) 
+text2 = """
+🔷🔹 Entrada Finalizada 🔹🔷
+     ✅✅ GRENN! ✅✅
+  """
 
+print("=====")
 
-
-   
-n_jogadas = random.randint(1, 12)
-n_jogadas2 = random.randint(1, 6)
-n_jogadas3 = random.randint(1, 6)
-n_jogadas4 = random.randint(1, 6)
-validade = datetime.datetime.now() + datetime.timedelta(minutes=2)
+possibilidade_mina_aleatoria = random.choice(possibilidades_minas)
+link_aleatorio = random.choice(links)
+validade = datetime.datetime.now() + datetime.timedelta(minutes=5)
 hora_validade = validade.strftime("%H:%M")
-mensagem_formatada = mensagem.format(n_jogadas,n_jogadas2,n_jogadas3,hora_validade, n_jogadas4)
-
+mensagem_formatada = mensagem.format(possibilidade_mina_aleatoria, hora_validade)
+mensagem_formatada = mensagem_formatada.replace("LINK_PLATAFORMA_CORRETA", link_aleatorio)
+mensagem_formatada = mensagem_formatada.replace("LINK_JOGO", link_aleatorio)
 bot.send_message(chat_id=group_id, text=mensagem_formatada, parse_mode='HTML', disable_web_page_preview=True)
-
-time.sleep(120) 
-
-bot.send_message(chat_id=group_id, text=texto5, parse_mode='HTML', disable_web_page_preview=True)
+   
+time.sleep(120)
+bot.send_message(chat_id=group_id, text=text2 ,parse_mode='HTML', disable_web_page_preview=True)
 bot.send_sticker(chat_id=group_id, sticker=sticker_file_id)
-time.sleep(120) 
+time.sleep(480)
