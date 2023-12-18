6269879baed7faef661432881f12eaf69a654395
severino tiburcio (TKI TECNOLOGIA) <46749707+severino6464@users.noreply.github.com>
2023-12-18 11:52:02 -0300

    Update sala917.py

--- sala917.py
+++ sala917.py
@@ -116,7 +116,7 @@ def enviar_sinais_e_relatorio():
 
 def check_and_send_signal():
     current_time = datetime.datetime.now().strftime("%H:%M")
-    signal_times = ["09:00", "15:00","20:00", "11:45"]
+    signal_times = ["09:00", "15:00","20:00", "11:55"]
 
     if current_time in signal_times:
         enviar_sinais_e_relatorio()
