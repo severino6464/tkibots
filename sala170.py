e7bc8dca710fda0f28f080786842ce0dc6dc3c29
severino tiburcio (TKI TECNOLOGIA) <46749707+severino6464@users.noreply.github.com>
2024-02-16 01:58:38 -0300

    Atualização do arquivo

--- sala170.py
+++ sala170.py
@@ -8,50 +8,16 @@ CHAVE_API = "5597794728:AAGfwOg3RijfPrQ5S_Iw6NKAuYucNEdIsO8" # BOT FOX
 bot = telebot.TeleBot(CHAVE_API)
 
 group_id = '-1001959863201'
+texto4 = """
 
-import telebot
-import time
-import datetime
-import random
-
-CHAVE_API = "5597794728:AAGfwOg3RijfPrQ5S_Iw6NKAuYucNEdIsO8" # BOT FOX
-
-bot = telebot.TeleBot(CHAVE_API)
-
-group_id = '-1001959863201'
+⚠️⚠️⚠️ ATENÇÃO PLATAFORMA EM LANÇAMENTO E TÃO DOBRANDO OS DEPÓSITOS ⚠️⚠️⚠️
 
 
+[📱 Cadastre-se aqui](https://vexbet.tech/)
+[LINK DA PLATAFORMA](https://vexbet.tech/)
 
-
-
-mensagem = """
-🚨 ENTRADA CONFIRMADA 🚨
-
-🐯 Fortune Tiger 
-⏰ Estratégia: Horários Pagantes
-⚠️ Válido ate: {}
-
-💰 {}x Normal
-💰 {}x Turbo
-
-⚡ Intercalando
-
-[🔗 Fazer CADASTRO ✅](https://affiliates.nuts.bet/visit/?bta=36058&brand=nutsbetaffiliates)
-[🔗 Abrir FORTUNE TIGER](https://nuts.bet/casino/game/2180615)
 
 """
 
-
-
-n_jogadas = random.randint(6, 20)
-n_jogadas2 = random.randint(4, 20)
-validade = datetime.datetime.now() + datetime.timedelta(minutes=10)
-hora_validade = validade.strftime("%H:%M")
-mensagem_formatada = mensagem.format(hora_validade,n_jogadas, n_jogadas2)
-
-bot.send_message(chat_id=group_id, text=mensagem_formatada, parse_mode='Markdown')
-
-time.sleep(600)  # Espera 10 minutos (600 segundos)
-
-
-
+bot.send_message(chat_id=group_id, text=texto4, parse_mode='Markdown')
+time.sleep(12000) 
