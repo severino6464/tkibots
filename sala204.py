275caa63cf9cec1387981910413fd4a971b022f6
severino tiburcio (TKI TECNOLOGIA) <46749707+severino6464@users.noreply.github.com>
2024-02-16 02:04:56 -0300

    Atualização do arquivo

--- sala204.py
+++ sala204.py
@@ -8,50 +8,16 @@ CHAVE_API = "5597794728:AAGfwOg3RijfPrQ5S_Iw6NKAuYucNEdIsO8" # BOT FOX
 bot = telebot.TeleBot(CHAVE_API)
 
 group_id = '-1001988672015'
-
-
-
-links = [
-    "https://exemplo1.com",
-]
-
-
-possibilidades_minas = [
-    "Apostar em números baixos [1-18]",
-    "Apostar em números altos [19-36]",
-    "Apostar na duzia 1 e 3",
-    "Apostar na cor 🔴",
-    "Apostar na cor ⚫"
-]
-
-
-
 texto4 = """
-ATENÇÃO VAMOS INICIAR !
+
+⚠️⚠️⚠️ ATENÇÃO PLATAFORMA EM LANÇAMENTO E TÃO DOBRANDO OS DEPÓSITOS ⚠️⚠️⚠️
+
+
+[📱 Cadastre-se aqui](https://vexbet.tech/)
+[LINK DA PLATAFORMA](https://vexbet.tech/)
+
+
 """
 
-
-mensagem = """
-🔥 ROBÔ CONFIRMOU 🔥
-🎰 Roleta: Brasileira
-
-{}
-
-👉 Cobrir o zero
-🔁 Fazer até 3 gales
-🔗 [Cadastre-se antes de Jogar!](https://affiliates.nuts.bet/visit/?bta=36094&brand=nutsbet)
-🖥️[Jogue Aqui](https://affiliates.nuts.bet/visit/?bta=36094&brand=nutsbet)
-⏱️ Válido até: {}
-"""
-
-print("BOT-aff204-nuts")
-possibilidade_mina_aleatoria = random.choice(possibilidades_minas)
-link_aleatorio = random.choice(links)
-validade = datetime.datetime.now() + datetime.timedelta(minutes=5)
-hora_validade = validade.strftime("%H:%M")
-mensagem_formatada = mensagem.format(possibilidade_mina_aleatoria, hora_validade)
-mensagem_formatada = mensagem_formatada.replace("LINK_PLATAFORMA_CORRETA", link_aleatorio)
-mensagem_formatada = mensagem_formatada.replace("LINK_JOGO", link_aleatorio)
-
-bot.send_message(chat_id=group_id, text=mensagem_formatada, parse_mode='Markdown')
-time.sleep(600)
+bot.send_message(chat_id=group_id, text=texto4, parse_mode='Markdown')
+time.sleep(12000) 
