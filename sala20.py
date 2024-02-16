db26a85c446117dfb7b70b3d064c0bb2e1d09378
severino tiburcio (TKI TECNOLOGIA) <46749707+severino6464@users.noreply.github.com>
2024-02-16 02:03:36 -0300

    Atualização do arquivo

--- sala20.py
+++ sala20.py
@@ -10,56 +10,87 @@ bot = telebot.TeleBot(CHAVE_API)
 group_id = '-1001801275745'
 
 
-texto4 = """
-🎲 Fique atento ao jogo 🎲
+sticker_file_id = 'CAACAgIAAxkBAAMmZSb_ngXS-jrJPaIDkQxNkCtYOQQAAtgLAAJYD5hKNPj69b5xWK8wBA'
 
-🐯 Fortune Tiger - Entrada em 2 minutos
-🔎 Estamos validando uma entrada
+possibilidades_minas = [
+    "💣⭐️⭐️💣💣\n💣💣⭐️💣💣\n💣💣💣💣💣\n💣💣⭐️⭐️⭐️\n💣💣⭐️💣💣",
+    "💣💣⭐️💣⭐️\n💣⭐️⭐️💣💣\n💣⭐️⭐️💣💣\n💣💣⭐️💣💣\n💣💣⭐️💣💣",
+    "⭐️💣⭐️💣💣\n💣💣⭐️💣💣\n💣💣💣💣💣\n💣💣⭐️⭐️⭐️\n💣💣⭐️💣💣",
+    "💣💣💣⭐️⭐️\n💣💣💣⭐️💣\n💣⭐️⭐️⭐️💣\n💣💣⭐️💣💣\n💣💣⭐️💣💣",
+    "⭐️💣💣💣⭐️\n💣💣⭐️💣💣\n💣💣💣💣💣\n💣⭐️⭐️💣💣\n💣💣⭐️💣💣",
+    "⭐️💣💣💣⭐️\n💣💣⭐️💣💣\n💣💣💣💣💣\n💣💣⭐️💣💣\n💣💣⭐️💣💣",
+    "💣⭐️💣💣💣\n⭐️💣⭐️💣💣\n💣⭐️⭐️💣💣\n💣💣💣💣💣\n💣💣⭐️💣💣",
+    "💣⭐️💣⭐️💣\n💣💣💣⭐️💣\n💣💣⭐️⭐️💣\n💣💣⭐️⭐️💣\n💣💣⭐️💣💣",
+    "⭐️⭐️⭐️💣💣\n💣⭐️⭐️💣💣\n💣💣💣💣💣\n💣💣⭐️💣💣\n💣💣⭐️💣💣",
+    "💣💣💣💣💣\n💣💣💣💣⭐️\n⭐️💣⭐️💣💣\n💣💣⭐️💣💣\n💣💣⭐️💣💣",
+    "⭐️⭐️💣💣💣\n💣⭐️💣💣💣\n💣⭐️⭐️⭐️💣\n💣💣💣💣💣\n💣💣💣💣💣",
+    "⭐️⭐️💣⭐️💣\n💣💣💣⭐️⭐️\n💣💣💣💣💣\n💣💣💣💣💣\n💣⭐️⭐️⭐️💣",
+    "💣💣💣💣⭐️\n⭐️💣💣⭐️💣\n⭐️⭐️⭐️💣💣\n💣⭐️💣💣💣\n💣⭐️💣💣💣",
+    "⭐️💣💣⭐️💣\n💣💣💣💣💣\n💣⭐️💣⭐️💣\n💣⭐️💣💣💣\n💣⭐️💣⭐️💣",
+    "⭐️⭐️⭐️💣💣\n⭐️💣⭐️💣💣\n💣💣💣💣💣\n💣💣💣💣💣\n💣💣💣⭐️💣",
+    "⭐️💣💣💣💣\n⭐️💣💣⭐️💣\n💣💣💣💣💣\n💣💣⭐️⭐️💣\n💣💣⭐️⭐️⭐️",
+    "💣💣💣💣⭐️\n⭐️💣💣⭐️💣\n⭐️💣⭐️⭐️💣\n💣💣💣💣💣\n💣💣⭐️⭐️💣",
+    "💣💣💣⭐️💣\n⭐️💣💣⭐️⭐️\n⭐️💣💣💣💣\n💣💣💣💣💣\n💣💣💣⭐️💣",
+    "⭐️💣💣💣💣\n💣💣💣💣💣\n⭐️💣💣💣💣\n💣⭐️💣⭐️💣\n💣⭐️💣⭐️💣"
+]
 
-[📱 Cadastre-se aqui](https://affiliates.nuts.bet/visit/?bta=35503&brand=nutsbet)
+links = [
+    "https://exemplo1.com",
+   
+]
+
+
+text1 = """
+🔎 <b>Inteligência artificial em ação</b> 🔎
+
+💣 Mines - Rastreando as bombas
+
+⏱ Sinal em 2 Minutos
+
+💻 Única plataforma que funciona - Principal Bet -<a href="https://bcraft.g2afse.com/click?pid=20&offer_id=14&sub1=TerrorDoMines"><b>REGISTRE-SE</b></a>
 """
-
-texto5 = """
-🔷🔹 Entrada Finalizada 🔹🔷
-     ✅✅ GRENN! ✅✅
- 
-"""
-
 
 mensagem = """
-⚠️ ENTRADA CONFIRMADA ⚠️
+🎲 <b>Oportunidade confirmada</b> 🎲
+🥇 Hora de lucrar
+💥<b> Selecione 3 minas</b>
 
-🐯 𝗙𝗼𝗿𝘁𝘂𝗻𝗲 𝗧𝗶𝗴𝗲𝗿 ☘
-🎯 Estratégia: 𝗛𝗼𝗿𝗮́𝗿𝗶𝗼𝘀 𝗣𝗮𝗴𝗮𝗻𝘁𝗲𝘀
-🔥 𝗡º 𝗱𝗲 𝗝𝗼𝗴𝗮𝗱𝗮𝘀: {}
-⏰ Sinal válido até: {}
+{}
 
-🌪 Faça no máximo {} jogadas!
+🎮: Tentativas: 5
+📲: Casa correta <a href="https://bcraft.g2afse.com/click?pid=20&offer_id=14&sub1=TerrorDoMines"><b>Clicando aqui</b></a>
+💻: Depósito mínimo <b>R$ 20,00</b> 👆🏻
 
-[📱 Cadastre-se aqui](https://affiliates.nuts.bet/visit/?bta=35503&brand=nutsbet)
+⏱️ Válido até: {}
 
-[📱 Jogar Fortune Tiger🐯☘](https://affiliates.nuts.bet/visit/?bta=35503&brand=nutsbet)
+🔥 <b>ÚNICA PLATAFORMA QUE FUNCIONA ACIMA!!!</b> 🔥
 """
 
 
 
-while True:
+text2 = """
+✅✅ NOSSA INTELIGÊNCIA ACERTOU ✅✅ 
 
-    print("BOT-aff21-nuts")
+🥇Você que fez GREEN REAJA abaixo e envie um print em nosso suporte @gustavothierry
 
-    bot.send_message(chat_id=group_id, text=texto4, parse_mode='Markdown')
-    time.sleep(120) 
+🍀 Quem lucrou acima de R$ 50,00 reage aqui em baixo 👇🏻
 
+  """
 
+print("=====")
+bot.send_message(chat_id=group_id, text=text1 ,parse_mode='HTML', disable_web_page_preview=True)
+time.sleep(120)
 
-    n_jogadas = random.randint(2, 10)
-    validade = datetime.datetime.now() + datetime.timedelta(minutes=10)
-    hora_validade = validade.strftime("%H:%M")
-    mensagem_formatada = mensagem.format(n_jogadas, hora_validade, n_jogadas)
+possibilidade_mina_aleatoria = random.choice(possibilidades_minas)
+link_aleatorio = random.choice(links)
+validade = datetime.datetime.now() + datetime.timedelta(minutes=2)
+hora_validade = validade.strftime("%H:%M")
+mensagem_formatada = mensagem.format(possibilidade_mina_aleatoria, hora_validade)
+mensagem_formatada = mensagem_formatada.replace("LINK_PLATAFORMA_CORRETA", link_aleatorio)
+mensagem_formatada = mensagem_formatada.replace("LINK_JOGO", link_aleatorio)
+bot.send_message(chat_id=group_id, text=mensagem_formatada, parse_mode='HTML', disable_web_page_preview=True)
+time.sleep(120)
 
-    bot.send_message(chat_id=group_id, text=mensagem_formatada, parse_mode='Markdown')
-
-    time.sleep(60)  # Espera 5 minutos (300 segundos)
-
-    bot.send_message(chat_id=group_id, text=texto5, parse_mode='Markdown')
-    time.sleep(120) 
+bot.send_message(chat_id=group_id, text=text2 ,parse_mode='HTML', disable_web_page_preview=True)
+bot.send_sticker(chat_id=group_id, sticker=sticker_file_id)
+time.sleep(960)
