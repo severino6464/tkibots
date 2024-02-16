bcef8989a0fba467917fb82b041e2d5b7afab09d
severino tiburcio (TKI TECNOLOGIA) <46749707+severino6464@users.noreply.github.com>
2024-02-16 02:03:46 -0300

    Atualização do arquivo

--- sala200.py
+++ sala200.py
@@ -9,6 +9,8 @@ bot = telebot.TeleBot(CHAVE_API)
 
 group_id = '-1001898755385'
 
+sticker_file_id = 'CAACAgIAAxkBAAMmZSb_ngXS-jrJPaIDkQxNkCtYOQQAAtgLAAJYD5hKNPj69b5xWK8wBA'
+
 possibilidades_minas = [
     "💣⭐️⭐️💣💣\n💣💣⭐️💣💣\n💣💣💣💣💣\n💣💣⭐️⭐️⭐️\n💣💣⭐️💣💣",
     "💣💣⭐️💣⭐️\n💣⭐️⭐️💣💣\n💣⭐️⭐️💣💣\n💣💣⭐️💣💣\n💣💣⭐️💣💣",
@@ -37,38 +39,57 @@ links = [
 ]
 
 
+text1 = """
+🔎 <b>Inteligência artificial em ação</b> 🔎
+
+💣 Mines - Rastreando as bombas
+
+⏱ Sinal em 2 Minutos
+
+💻 Única plataforma que funciona - Principal Bet -<a href="https://bcraft.g2afse.com/click?pid=20&offer_id=14&sub1=TerrorDoMines"><b>REGISTRE-SE</b></a>
+"""
 
 mensagem = """
-🎲 Entrada confirmada 🎲
-🥇: Entrada 
+🎲 <b>Oportunidade confirmada</b> 🎲
+🥇 Hora de lucrar
+💥<b> Selecione 3 minas</b>
 
 {}
-🎮: Tentativas: 2
-Jogar com 2 a 3 minas
 
-📲: Plataforma correta: [Clique aqui](https://affiliates.nuts.bet/visit/?bta=36087&brand=nutsbet)
-👉🏻: Link do jogo: [Mines](https://affiliates.nuts.bet/visit/?bta=36087&brand=nutsbet)
+🎮: Tentativas: 5
+📲: Casa correta <a href="https://bcraft.g2afse.com/click?pid=20&offer_id=14&sub1=TerrorDoMines"><b>Clicando aqui</b></a>
+💻: Depósito mínimo <b>R$ 20,00</b> 👆🏻
+
 ⏱️ Válido até: {}
 
+🔥 <b>ÚNICA PLATAFORMA QUE FUNCIONA ACIMA!!!</b> 🔥
 """
 
 
+
 text2 = """
-🔷🔹 Entrada Finalizada 🔹🔷
-     ✅✅ GRENN! ✅✅
+✅✅ NOSSA INTELIGÊNCIA ACERTOU ✅✅ 
+
+🥇Você que fez GREEN REAJA abaixo e envie um print em nosso suporte @gustavothierry
+
+🍀 Quem lucrou acima de R$ 50,00 reage aqui em baixo 👇🏻
+
   """
 
-print("aff200")
+print("=====")
+bot.send_message(chat_id=group_id, text=text1 ,parse_mode='HTML', disable_web_page_preview=True)
+time.sleep(120)
 
 possibilidade_mina_aleatoria = random.choice(possibilidades_minas)
 link_aleatorio = random.choice(links)
-validade = datetime.datetime.now() + datetime.timedelta(minutes=5)
+validade = datetime.datetime.now() + datetime.timedelta(minutes=2)
 hora_validade = validade.strftime("%H:%M")
 mensagem_formatada = mensagem.format(possibilidade_mina_aleatoria, hora_validade)
 mensagem_formatada = mensagem_formatada.replace("LINK_PLATAFORMA_CORRETA", link_aleatorio)
 mensagem_formatada = mensagem_formatada.replace("LINK_JOGO", link_aleatorio)
-bot.send_message(chat_id=group_id, text=mensagem_formatada, parse_mode='Markdown')
-    
+bot.send_message(chat_id=group_id, text=mensagem_formatada, parse_mode='HTML', disable_web_page_preview=True)
 time.sleep(120)
-bot.send_message(chat_id=group_id, text=text2 ,parse_mode='Markdown')
-time.sleep(300)  # Espera 5 minutos (300 segundos)
+
+bot.send_message(chat_id=group_id, text=text2 ,parse_mode='HTML', disable_web_page_preview=True)
+bot.send_sticker(chat_id=group_id, sticker=sticker_file_id)
+time.sleep(960)
